f1e9eeabc1b7d14236e132be9b533fbbc547aa43
Phil Evans <phil.evans@thoughtworks.com>
2019-12-02 17:38:44 +0000

    [PRMT-760] Pipeline for Deductions Core

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -3,7 +3,7 @@ locals {
 }
 resource "aws_route53_record" "gp2gp-r53-record" {
   zone_id = data.aws_ssm_parameter.root_zone_id.value
-  name    = "dev.gp2gp"
+  name    = "dev.gp2gp-adaptor"
   type    = "CNAME"
   ttl     = "300"
   records = [local.deductions_private_alb_dns]
