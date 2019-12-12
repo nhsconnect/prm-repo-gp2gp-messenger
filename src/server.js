60f16beb27badfbe9baa9eaa8702349093633615
Amy Welch <awelch@thoughtworks.com>
2019-12-12 15:12:47 +0000

    [PRMT-505] initialise queue consumer on starting server

--- src/server.js
+++ src/server.js
@@ -1,4 +1,7 @@
 import app from './app';
 import logger from './config/logging';
+import initialiseConsumer from './services/consumer';
+
+initialiseConsumer();
 
 app.listen(3000, () => logger.info('Listening on port 3000'));
