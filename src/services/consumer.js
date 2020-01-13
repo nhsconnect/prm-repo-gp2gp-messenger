1f42328cf9ec0260f0e57ca75227536ac9422b87
bfisher <fishey2b@gmail.com>
2020-01-13 12:00:03 +0000

    [PRMT-541] Refactored consumer.js in preparation for use of transactions

--- src/services/consumer.js
+++ src/services/consumer.js
@@ -1,77 +1,103 @@
 import config from '../config';
 import handleMessage from './message-handler';
+import { getCorrelationId } from '../middleware/correlation';
+import { connectToQueue } from '../config/queue';
 import {
   eventFinished,
   updateLogEvent,
   updateLogEventWithError,
   withContext
 } from '../middleware/logging';
-import { getCorrelationId } from '../middleware/correlation';
-import { connectToQueue } from '../config/queue';
+
+// Transactions
+// client.begin([headers])
+// transaction.send(headers, [options])
+// transaction.commit([options])
+// option: onReceipt get confirmation from the server that this transaction was successfully committed.
+// The server may terminate the connection with an error frame if it cannot commit the transaction. In this case, an error event would be emitted from the client object.
+// transaction.abort([options])
 
 const sendMessageToDlq = (client, body, error) => {
   updateLogEvent({ status: 'message-sent-to-dlq' });
 
-  const frame = client.send({
-    destination: config.dlqName,
-    errorMessage: error.message,
-    stackTrace: error.stack,
-    correlationId: getCorrelationId()
-  });
+  // client.send(headers, [options]) stream.Writable
+  // alt = client.sendFrame(command, headers, [options]): stream.Writable
+  // ack: set the message acknowledgment mode, having value 'auto', 'client' or 'client-individual'
+  const frame = client.send(getHeaders(error));
   frame.write(body);
   frame.end();
 };
 
-const streamMessageToDlq = (client, msg, error) => {
+const streamMessageToDlq = (client, message, error) => {
   updateLogEvent({ status: 'message-sent-to-dlq' });
 
-  const frame = client.send({
+  const frame = client.send(getHeaders(error));
+  message.pipe(frame);
+};
+
+const getHeaders = (error, transaction) => {
+  const standardSendHeaders = {
     destination: config.dlqName,
     errorMessage: error.message,
     stackTrace: error.stack,
     correlationId: getCorrelationId()
+  };
+
+  return transaction
+    ? {
+        ...standardSendHeaders,
+        transation: transaction.id
+      }
+    : standardSendHeaders;
+};
+
+const readMessageCallback = (client, message) => (err, body) => {
+  if (err) {
+    updateLogEventWithError(err);
+    body ? sendMessageToDlq(client, body, err) : streamMessageToDlq(client, message, err);
+    eventFinished();
+    return;
+  }
+
+  handleMessage(body)
+    .then(() => {
+      // Acknowledges - no body
+      message.ack();
+      updateLogEvent({ status: 'message-handled' });
+    })
+    .catch(err => {
+      updateLogEventWithError(err);
+      sendMessageToDlq(client, body, err);
+      // Not Acknowledges - no body
+      // If you can't read message use client.destroy
+      message.nack();
+    })
+    .finally(eventFinished);
+};
+
+const subscribeCallback = client => (err, message) => {
+  withContext(() => {
+    updateLogEvent({ status: 'consuming-message' });
+
+    if (err) {
+      updateLogEventWithError(err);
+      if (!message) throw err;
+      streamMessageToDlq(client, message, err);
+      eventFinished();
+      return;
+    }
+    message.readString('UTF-8', readMessageCallback(client, message));
   });
-  msg.pipe(frame);
 };
 
 const initialiseConsumer = () => {
   connectToQueue((err, client) => {
     if (err) throw err;
 
-    client.subscribe({ destination: config.queueName }, (err, message) => {
-      withContext(() => {
-        updateLogEvent({ status: 'consuming-message' });
-
-        if (err) {
-          updateLogEventWithError(err);
-          if (!message) throw err;
-          streamMessageToDlq(client, message, err);
-          eventFinished();
-          return;
-        }
-
-        message.readString('UTF-8', (err, body) => {
-          if (err) {
-            updateLogEventWithError(err);
-            body ? sendMessageToDlq(client, body, err) : streamMessageToDlq(client, message, err);
-            eventFinished();
-            return;
-          }
-
-          handleMessage(body)
-            .then(() => {
-              message.ack();
-              updateLogEvent({ status: 'message-handled' });
-            })
-            .catch(err => {
-              updateLogEventWithError(err);
-              sendMessageToDlq(client, body, err);
-              message.nack();
-            })
-            .finally(eventFinished);
-        });
-      });
-    });
+    // client.subscribe(headers, onMessageCallback)
+    // onMessageCallback(error, message)
+    // message<T extends stream.Readable>
+    client.subscribe({ destination: config.queueName }, subscribeCallback(client));
   });
 };
 
