d9345af328fa8c13dbc7c20dd2ebcce01dd74813
Helen Zhou <helen.zhou@thoughtworks.com>
2020-02-19 10:46:54 +0000

    [PRMT-962] Fix ehr-request-test to not rely on testData instead of config causing pipeline issues

--- src/services/ehr-request.test.js
+++ src/services/ehr-request.test.js
@@ -2,6 +2,7 @@ import { when } from 'jest-when';
 import config from '../config';
 import { updateLogEvent } from '../middleware/logging';
 import generateEhrRequestQuery from '../templates/ehr-request-template';
+import testData from '../templates/tests/testData.json';
 import sendEhrRequest from './ehr-request';
 import * as mhsGateway from './mhs-gateway';
 import * as mhsGatewayFake from './mhs-gateway-fake';
@@ -13,24 +14,36 @@ jest.mock('uuid/v4', () => () => 'some-uuid');
 jest.mock('moment', () => () => ({ format: () => '20190228112548' }));
 
 describe('sendEhrRequest', () => {
-  const odsCode = 'some-ods-code';
-  const receivingAsid = 'some-asid';
-  const nhsNumber = 'some-nhs-number';
-  const ehrRequestQuery = generateEhrRequestQuery({
-    id: 'some-uuid',
-    timestamp: '20190228112548',
-    receivingService: {
-      asid: receivingAsid,
-      odsCode: odsCode
-    },
-    sendingService: {
-      asid: config.deductionsAsid,
-      odsCode: config.deductionsOdsCode
-    },
-    patient: {
-      nhsNumber: nhsNumber
-    }
+  let ehrRequestQuery;
+  beforeEach(() => {
+    config.deductionsAsid = 'some-asid';
+    config.odsCode = 'some-ods-code';
+
+    ehrRequestQuery = generateEhrRequestQuery({
+      id: 'some-uuid',
+      timestamp: '20190228112548',
+      receivingService: {
+        asid: receivingAsid,
+        odsCode: odsCode
+      },
+      sendingService: {
+        asid: config.deductionsAsid,
+        odsCode: config.deductionsOdsCode
+      },
+      patient: {
+        nhsNumber: nhsNumber
+      }
+    });
   });
+
+  afterEach(() => {
+    config.deductionsAsid = process.env.DEDUCTIONS_ASID;
+    config.deductionsOdsCode = process.env.DEDUCTIONS_ODS_CODE;
+  });
+
+  const odsCode = testData.emisPractise.odsCode;
+  const receivingAsid = testData.emisPractise.asid;
+  const nhsNumber = testData.emisPatient.nhsNumber;
 
   it('should send generated EHR request message to real MHS when environment is PTL', () => {
     config.isPTL = true;
@@ -62,7 +75,7 @@ describe('sendEhrRequest', () => {
   });
 
   it('should update log event for each stage', () => {
-    mhsGatewayFake.getRoutingInformation.mockResolvedValue({ asid: 'some-asid' });
+    mhsGatewayFake.getRoutingInformation.mockResolvedValue({ asid: receivingAsid });
 
     return sendEhrRequest(nhsNumber, odsCode).then(() => {
       expect(updateLogEvent).toHaveBeenCalledWith({
@@ -71,7 +84,7 @@ describe('sendEhrRequest', () => {
       });
       expect(updateLogEvent).toHaveBeenCalledWith({
         status: 'requesting-ehr',
-        ehrRequest: { asid: 'some-asid' }
+        ehrRequest: { asid: receivingAsid }
       });
       expect(updateLogEvent).toHaveBeenCalledWith({ status: 'requested-ehr' });
     });
