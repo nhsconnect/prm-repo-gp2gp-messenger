f234bc8a12718f69719b78eb825c12ff78def118
Helen Zhou <helen.zhou@thoughtworks.com>
2020-02-24 16:19:01 +0000

    [PRMT-993] Add a message payload to MHS Service with validation

--- src/services/mhs-service.js
+++ src/services/mhs-service.js
@@ -1,12 +1,13 @@
 import axios from 'axios';
 import { updateLogEventWithError } from '../middleware/logging';
 
-const validateInputs = ({ interactionId, conversationId }) => {
-  if (interactionId && conversationId) return;
+const validateInputs = ({ interactionId, conversationId, message }) => {
+  if (interactionId && conversationId && message) return;
 
   const errorMessages = [];
   if (!interactionId) errorMessages.push('interactionId must be passed in');
   if (!conversationId) errorMessages.push('conversationId must be passed in');
+  if (!message) errorMessages.push('message must be passed in');
 
   const error = new Error(errorMessages);
 
@@ -14,23 +15,34 @@ const validateInputs = ({ interactionId, conversationId }) => {
   throw error;
 };
 
-const sendMessage = ({ interactionId, conversationId, odsCode = 'YES' } = {}) => {
-  return new Promise(resolve => {
-    validateInputs({ interactionId, conversationId });
+const processXmlMessage = (xml = '') =>
+  xml
+    .trim()
+    .replace(/\r?\n|\r/g, '')
+    .replace(/>\s+</g, '><');
 
-    return axios({
-      method: 'POST',
-      url:
+const sendMessage = ({ interactionId, conversationId, odsCode = 'YES', message } = {}) => {
+  return new Promise(resolve => {
+    validateInputs({ interactionId, conversationId, message });
+
+    return axios
+      .post(
         'https://gpitbjss.atlassian.net/wiki/spaces/TW/pages/1802240048/MHS+Adapter+GP2GP+Compliance',
-      headers: {
-        'Content-Type': 'application/json',
-        'Interaction-ID': interactionId,
-        'Sync-Async': false,
-        'Correlation-Id:': conversationId,
-        'Ods-Code': odsCode
-      }
-    }).then(resolve);
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            'Interaction-ID': interactionId,
+            'Sync-Async': false,
+            'Correlation-Id:': conversationId,
+            'Ods-Code': odsCode
+          },
+          data: {
+            payload: processXmlMessage(message)
+          }
+        }
+      )
+      .then(resolve);
   });
 };
 
-export { sendMessage };
+export { sendMessage, processXmlMessage };
--- src/services/tests/mhs-service.test.js
+++ src/services/tests/mhs-service.test.js
@@ -1,50 +1,59 @@
 import axios from 'axios';
-import { when } from 'jest-when';
+import dateFormat from 'dateformat';
 import uuid from 'uuid/v4';
 import { updateLogEventWithError } from '../../middleware/logging';
-import { sendMessage } from '../mhs-service';
+import generatePdsRetrievalQuery from '../../templates/generate-pds-retrieval-request';
+import testData from '../../templates/tests/testData.json';
+import { processXmlMessage, sendMessage } from '../mhs-service';
 
 jest.mock('../../config/logging');
 jest.mock('../../middleware/logging');
 
 jest.mock('axios');
 
+const conversationId = uuid().toUpperCase();
+const timestamp = dateFormat(Date.now(), 'yyyymmddHHMMss');
+const interactionId = 'QUPA_IN040000UK32';
+
 describe('mhs-service', () => {
-  const interactionId = 'QUPA_IN000008UK02';
-  const conversationId = uuid();
-  const odsCode = 'A123';
+  const pdsRetrievalQuery = generatePdsRetrievalQuery({
+    id: conversationId,
+    timestamp,
+    receivingService: { asid: testData.pds.asid },
+    sendingService: { asid: testData.mhs.asid },
+    patient: { nhsNumber: testData.emisPatient.nhsNumber }
+  });
+
   const requestOptions = {
-    method: 'POST',
-    url:
-      'https://gpitbjss.atlassian.net/wiki/spaces/TW/pages/1802240048/MHS+Adapter+GP2GP+Compliance',
     headers: {
       'Content-Type': 'application/json',
       'Interaction-ID': interactionId,
       'Sync-Async': false,
       'Correlation-Id:': conversationId,
-      'Ods-Code': odsCode
+      'Ods-Code': 'YES'
+    },
+    data: {
+      payload: processXmlMessage(pdsRetrievalQuery)
     }
   };
 
   beforeEach(() => {
     jest.resetAllMocks();
-
-    when(axios)
-      .calledWith(requestOptions)
-      .mockResolvedValue({ status: 200 })
-      .calledWith({
-        ...requestOptions,
-        headers: {
-          ...requestOptions.headers,
-          'Ods-Code': 'YES'
-        }
-      })
-      .mockResolvedValue({ status: 200 });
+    axios.post.mockResolvedValue(Promise.resolve({ status: 200 }));
   });
 
   it('logs an Error if interactionId is not passed in', () => {
-    return sendMessage({ conversationId }).catch(err => {
+    return sendMessage({ conversationId, message: pdsRetrievalQuery }).catch(err => {
       const error = Error(['interactionId must be passed in']);
+      expect(err).toEqual(error);
+      expect(updateLogEventWithError).toHaveBeenCalledTimes(1);
+      expect(updateLogEventWithError).toHaveBeenCalledWith(error);
+    });
+  });
+
+  it('logs an Error if message is not passed in', () => {
+    return sendMessage({ conversationId, interactionId }).catch(err => {
+      const error = Error(['message must be passed in']);
       expect(err).toEqual(error);
       expect(updateLogEventWithError).toHaveBeenCalledTimes(1);
       expect(updateLogEventWithError).toHaveBeenCalledWith(error);
@@ -53,38 +62,92 @@ describe('mhs-service', () => {
 
   it('logs an Error if conversationId is not passed in', () => {
     const error = Error(['conversationId must be passed in']);
-    return sendMessage({ interactionId }).catch(err => {
+    return sendMessage({ interactionId, message: pdsRetrievalQuery }).catch(err => {
       expect(err).toEqual(error);
       expect(updateLogEventWithError).toHaveBeenCalledTimes(1);
       expect(updateLogEventWithError).toHaveBeenCalledWith(error);
     });
   });
 
-  it('logs an Error if conversationId and interactionId is not passed in', () => {
+  it('logs an Error if conversationId, message and interactionId is not passed in', () => {
     return sendMessage().catch(err => {
       expect(err.message).toContain('conversationId must be passed in');
       expect(err.message).toContain('interactionId must be passed in');
+      expect(err.message).toContain('message must be passed in');
       expect(updateLogEventWithError).toHaveBeenCalledTimes(1);
       expect(updateLogEventWithError).toHaveBeenCalledWith(expect.any(Error));
     });
   });
 
   it('throws an Error if interactionId is not passed in', () => {
-    expect(sendMessage({ conversationId })).rejects.toThrowError(
+    return expect(sendMessage({ conversationId, message: pdsRetrievalQuery })).rejects.toThrowError(
       Error('interactionId must be passed in')
     );
   });
 
   it("should call axios with odsCode 'YES' by default and return 200", () => {
-    return sendMessage({ interactionId, conversationId }).then(response => {
-      expect(response.status).toBe(200);
-      expect(axios).toBeCalledWith({
-        ...requestOptions,
-        headers: {
-          ...requestOptions.headers,
-          'Ods-Code': 'YES'
-        }
-      });
-    });
+    return sendMessage({ interactionId, conversationId, message: pdsRetrievalQuery }).then(
+      response => {
+        expect(response.status).toBe(200);
+        expect(axios.post).toBeCalledWith(
+          'https://gpitbjss.atlassian.net/wiki/spaces/TW/pages/1802240048/MHS+Adapter+GP2GP+Compliance',
+          requestOptions
+        );
+      }
+    );
+  });
+
+  it('should call axios with specified odsCode if passed in', () => {
+    const odsCode = 'A123';
+    return sendMessage({ interactionId, conversationId, message: pdsRetrievalQuery, odsCode }).then(
+      response => {
+        expect(response.status).toBe(200);
+        expect(axios.post).toBeCalledWith(
+          'https://gpitbjss.atlassian.net/wiki/spaces/TW/pages/1802240048/MHS+Adapter+GP2GP+Compliance',
+          {
+            ...requestOptions,
+            headers: { ...requestOptions.headers, 'Ods-Code': odsCode }
+          }
+        );
+      }
+    );
+  });
+
+  it('should stringify and escape the payload (xml message)', () => {
+    return sendMessage({ interactionId, conversationId, message: pdsRetrievalQuery }).then(
+      response => {
+        expect(response.status).toBe(200);
+        expect(axios.post).toBeCalledWith(
+          'https://gpitbjss.atlassian.net/wiki/spaces/TW/pages/1802240048/MHS+Adapter+GP2GP+Compliance',
+          requestOptions
+        );
+      }
+    );
+  });
+});
+
+describe('processXmlMessage', () => {
+  const pdsRetrievalQuery = generatePdsRetrievalQuery({
+    id: conversationId,
+    timestamp,
+    receivingService: { asid: testData.pds.asid },
+    sendingService: { asid: testData.mhs.asid },
+    patient: { nhsNumber: testData.emisPatient.nhsNumber }
+  });
+
+  it('should return a string', () => {
+    expect(typeof processXmlMessage(pdsRetrievalQuery) === 'string').toBe(true);
+  });
+
+  it('should remove spaces between xml tags', () => {
+    expect(processXmlMessage(pdsRetrievalQuery).match(/> +</g)).toBe(null);
+  });
+
+  it('should trim the message', () => {
+    expect(processXmlMessage(` ${pdsRetrievalQuery}     `).match(/^ +| +$/)).toBe(null);
+  });
+
+  it('should remove new lines (/n) and carriage return characters (/r)', () => {
+    expect(processXmlMessage(pdsRetrievalQuery).match(/\r?\n|\r/g)).toBe(null);
   });
 });
