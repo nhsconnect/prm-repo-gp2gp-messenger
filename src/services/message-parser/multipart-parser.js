d7beb35c3ac9aa6c36eb8ac7a51d541da0542049
Wendy Lee <wendy.lee@thoughtworks.com>
2020-02-26 17:20:54 +0000

    [PRMT-986] Added multipart parser for multipart message

---
+++ src/services/message-parser/multipart-parser.js
@@ -0,0 +1,50 @@
+const boundaryMatcher = /^.*--=_MIME-Boundary$/gm;
+
+// Extracts key value pair based on {Key} : OWS {Value} OWS
+// Where the key include alpha characters and hyphens only
+// and the value can include any character.
+const headerMatcher = /^([a-zA-Z-]+):[\s]*(.*)[\s]*$/gm;
+const keyValuePairMatcher = /([a-zA-Z-]+):[\s]*(.*)[\s]*/;
+
+const newLineMatcher = /^[\n]+$/g;
+const bodyMatcher = /^[\s]*(.*)[\s]*$/gm;
+
+const getFullBoundaryText = multipartMessage => multipartMessage.match(boundaryMatcher) || '';
+
+const extractBoundaryContent = rawMultipartMessage => {
+  const messageParts = rawMultipartMessage
+    .trim()
+    .split(getFullBoundaryText(rawMultipartMessage)[0]);
+
+  return messageParts[messageParts.length - 1] === '--'
+    ? messageParts.slice(1, -1)
+    : messageParts.slice(1);
+};
+
+const extractHeaders = messagePart => extractKeyValuePairs(messagePart.match(headerMatcher) || []);
+
+const extractKeyValuePairs = headersArray =>
+  headersArray.reduce((headerObj, header) => {
+    const headerKeyPair = header.match(keyValuePairMatcher);
+
+    headerObj[headerKeyPair[1]] = headerKeyPair[2];
+    return headerObj;
+  }, {});
+
+const extractBody = messagePart => {
+  return messagePart
+    .match(bodyMatcher)
+    .map(line => (line.match(newLineMatcher) || line.match(keyValuePairMatcher) ? '' : line.trim()))
+    .join('');
+};
+
+const parseMultipartBody = multipartMessage => {
+  if (!multipartMessage) return [];
+
+  return extractBoundaryContent(multipartMessage).map(messagePart => ({
+    headers: extractHeaders(messagePart),
+    body: extractBody(messagePart)
+  }));
+};
+
+export { parseMultipartBody };
---
+++ src/services/message-parser/multipart-parser.test.js
@@ -0,0 +1,200 @@
+import { parseMultipartBody } from './multipart-parser';
+
+const testBody = `<body>something</body>`;
+const contentId = `<ebXMLHeader@spine.nhs.uk>`;
+const contentType = `text/xml`;
+const contentTransferEncoding = `8bit`;
+const authorization = 'Basic 123';
+const emptyMultipartMessage = ``;
+const mimeBoundaryMultipartMessage = `
+----=_MIME-Boundary
+
+----=_MIME-Boundary--
+`;
+
+const messageHasNoBody = `
+----=_MIME-Boundary
+Content-Id: ${contentId}
+Content-Type: ${contentType}
+Content-Transfer-Encoding: ${contentTransferEncoding}
+
+----=_MIME-Boundary--
+`;
+
+const messageHasNoHeader = `
+----=_MIME-Boundary
+${testBody}
+
+----=_MIME-Boundary--
+`;
+
+const messageHeaderWithAdditionalWhitespace = `
+----=_MIME-Boundary
+Content-Id:   ${contentId}
+
+----=_MIME-Boundary--
+`;
+
+const messageHeaderWithWhitespaceInValue = `
+----=_MIME-Boundary
+Authorization: ${authorization}
+
+----=_MIME-Boundary--
+`;
+
+const stringBodyMultipartMessage = `     some string        `;
+
+const oneObjectMultipartMessage = `
+----=_MIME-Boundary
+Content-Id: ${contentId}
+
+${testBody}
+
+----=_MIME-Boundary--
+`;
+
+const manyObjectsPathMultipartMessage = `
+----=_MIME-Boundary
+Content-Id: ${contentId}
+Content-Type: ${contentType}
+Content-Transfer-Encoding: ${contentTransferEncoding}
+
+${testBody}
+
+----=_MIME-Boundary
+Content-Id: <ebXMLHeader@spine.nhs.uk>
+Content-Type: text/xml
+Content-Transfer-Encoding: 8bit
+
+${testBody}
+
+----=_MIME-Boundary
+Content-Id: <ebXMLHeader@spine.nhs.uk>
+Content-Type: text/xml
+Content-Transfer-Encoding: 8bit
+
+${testBody}
+
+----=_MIME-Boundary--
+`;
+
+const noEofBoundaryMultipartMessage = `
+----=_MIME-Boundary
+Content-Id: <ebXMLHeader@spine.nhs.uk>
+
+${testBody}`;
+
+const message1 = {
+  headers: {
+    'Content-Id': contentId,
+    'Content-Type': contentType,
+    'Content-Transfer-Encoding': contentTransferEncoding
+  },
+  body: ''
+};
+
+const multipartMessageOutput = [
+  {
+    headers: {
+      'Content-Id': contentId,
+      'Content-Type': contentType,
+      'Content-Transfer-Encoding': contentTransferEncoding
+    },
+    body: testBody
+  },
+  {
+    headers: {
+      'Content-Id': contentId,
+      'Content-Type': contentType,
+      'Content-Transfer-Encoding': contentTransferEncoding
+    },
+    body: testBody
+  },
+  {
+    headers: {
+      'Content-Id': contentId,
+      'Content-Type': contentType,
+      'Content-Transfer-Encoding': contentTransferEncoding
+    },
+    body: testBody
+  }
+];
+
+describe('MultipartParser', () => {
+  describe('parseMultipartBody', () => {
+    it('should return empty array if the message is empty ', () => {
+      expect(parseMultipartBody(emptyMultipartMessage)).toEqual([]);
+    });
+
+    it('should return an array with empty object if the message only have boundary', () => {
+      expect(parseMultipartBody(mimeBoundaryMultipartMessage)).toEqual([
+        {
+          body: '',
+          headers: {}
+        }
+      ]);
+    });
+
+    it('should return object with header and empty body if message body is empty', () => {
+      expect(parseMultipartBody(messageHasNoBody)).toEqual([message1]);
+    });
+    // it('should return object with header and body', () => {
+    //   expect(parseMultipartBody(oneObjectMultipartMessage)).toEqual([message2]);
+    // });
+    it('should return empty array if there is no boundary in message', () => {
+      expect(parseMultipartBody(stringBodyMultipartMessage)).toEqual([]);
+    });
+    it('should trim header value if message header value has space in prefix ', () => {
+      expect(parseMultipartBody(messageHeaderWithAdditionalWhitespace)).toEqual([
+        {
+          headers: {
+            'Content-Id': contentId
+          },
+          body: ''
+        }
+      ]);
+    });
+    it('should not remove whitespace in the middle of header value', () => {
+      expect(parseMultipartBody(messageHeaderWithWhitespaceInValue)).toEqual([
+        {
+          headers: {
+            Authorization: authorization
+          },
+          body: ''
+        }
+      ]);
+    });
+
+    it('should get body from message', () => {
+      expect(parseMultipartBody(oneObjectMultipartMessage)).toEqual([
+        {
+          headers: {
+            'Content-Id': contentId
+          },
+          body: testBody
+        }
+      ]);
+    });
+    it('should return object with header and body if the message have many parts', () => {
+      expect(parseMultipartBody(manyObjectsPathMultipartMessage)).toEqual(multipartMessageOutput);
+    });
+    it('should return object with empty header if the message has not header ', () => {
+      expect(parseMultipartBody(messageHasNoHeader)).toEqual([
+        {
+          headers: {},
+          body: testBody
+        }
+      ]);
+    });
+    it('should return object with header and body if the end boundary is missing', () => {
+      expect(parseMultipartBody(noEofBoundaryMultipartMessage)).toEqual([
+        {
+          headers: {
+            'Content-Id': contentId
+          },
+          body: testBody
+        }
+      ]);
+    });
+  });
+});
