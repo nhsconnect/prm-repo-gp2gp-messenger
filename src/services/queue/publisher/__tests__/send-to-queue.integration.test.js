c5e80693577d15e472b72a6c43e1bbe66d9f925d
Helen Zhou <helen.zhou@thoughtworks.com>
2020-03-27 15:19:42 +0000

    [PRMT-1038] Adds integration test for having two queues and sending messages to both

--- src/services/queue/publisher/__tests__/send-to-queue.integration.test.js
+++ src/services/queue/publisher/__tests__/send-to-queue.integration.test.js
@@ -1,17 +1,37 @@
 import { clearQueue, consumeOneMessage, sendToQueue } from '../../';
-import { generateEhrExtractResponse } from '../../../../templates/soap/ehr-extract-template';
+import config from '../../../../config';
 
 jest.unmock('stompit');
+
+const firstMockMessage = 'message';
+const secondMockMessage = 'another-message';
 
 describe('sendToQueue', () => {
   afterEach(async () => {
     await clearQueue();
+    await clearQueue(config.unhandledMessagesQueueName);
   });
 
   it('should put a message on the queue that can then be consumed', async done => {
-    await sendToQueue(generateEhrExtractResponse());
+    await sendToQueue(firstMockMessage);
     const message = await consumeOneMessage();
-    expect(message).toEqual(generateEhrExtractResponse());
+    expect(message).toEqual(firstMockMessage);
     done();
+  });
+
+  describe('having two queues up', () => {
+    it('should send messages to correct destination', async done => {
+      await sendToQueue(firstMockMessage);
+      await sendToQueue(secondMockMessage, { destination: config.unhandledMessagesQueueName });
+
+      const messageOnDefaultQueue = await consumeOneMessage();
+      const uhandledMessage = await consumeOneMessage({
+        destination: config.unhandledMessagesQueueName
+      });
+
+      expect(messageOnDefaultQueue).toBe(firstMockMessage);
+      expect(uhandledMessage).toBe(secondMockMessage);
+      done();
+    });
   });
 });
