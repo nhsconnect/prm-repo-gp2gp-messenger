59c73add13a72a0332034be92f9a05507733dce4
Helen Zhou <helen.zhou@thoughtworks.com>
2020-03-31 15:54:09 +0100

    [PRMT-1038] Add logging to putMessageOnQueue

--- src/services/queue/publisher/__tests__/put-message-on-queue.test.js
+++ src/services/queue/publisher/__tests__/put-message-on-queue.test.js
@@ -1,44 +1,98 @@
 import config from '../../../../config';
+import { updateLogEvent } from '../../../../middleware/logging';
 import { mockClient, mockStream, mockTransaction } from '../../../../__mocks__/stompit';
 import { putMessageOnQueue } from '../put-message-on-queue';
 
-const MOCK_MESSAGE = 'message';
-const MOCK_QUEUE_NAME = 'mocked-queue-name';
+const mockMessage = 'message';
+const mockQueueName = 'mocked-queue-name';
+
+jest.mock('../../../../middleware/logging');
 
 describe('putMessageOnQueue', () => {
   beforeEach(() => {
-    config.queueName = MOCK_QUEUE_NAME;
+    config.queueName = mockQueueName;
   });
 
   afterEach(() => {
-    config.queueName = MOCK_QUEUE_NAME;
+    config.queueName = mockQueueName;
+  });
+
+  it('should call updateLogEvent with status "Putting Message on queue"', () => {
+    putMessageOnQueue(mockClient, mockMessage);
+    expect(updateLogEvent).toHaveBeenCalledWith(
+      expect.objectContaining({
+        status: 'Putting Message on queue'
+      })
+    );
+  });
+
+  it('should call updateLogEvent with queue options', () => {
+    const mockOptions = {
+      option: 'mock'
+    };
+    putMessageOnQueue(mockClient, mockMessage, mockOptions);
+    expect(updateLogEvent).toHaveBeenCalledWith(
+      expect.objectContaining({
+        queue: expect.objectContaining({
+          options: expect.objectContaining(mockOptions)
+        })
+      })
+    );
   });
 
   it('should call client.begin once', () => {
-    putMessageOnQueue(mockClient, MOCK_MESSAGE);
+    putMessageOnQueue(mockClient, mockMessage);
     expect(mockClient.begin).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call updateLogEvent with "Send transaction"', () => {
+    const mockOptions = {
+      option: 'mock'
+    };
+    putMessageOnQueue(mockClient, mockMessage, mockOptions);
+    expect(updateLogEvent).toHaveBeenCalledWith(
+      expect.objectContaining({
+        status: 'Sending transaction'
+      })
+    );
+  });
+
+  it('should call updateLogEvent with transaction id', () => {
+    const mockOptions = {
+      option: 'mock'
+    };
+    putMessageOnQueue(mockClient, mockMessage, mockOptions);
+    expect(updateLogEvent).toHaveBeenCalledWith(
+      expect.objectContaining({
+        queue: expect.objectContaining({
+          transaction: expect.objectContaining({
+            id: mockTransaction.id
+          })
+        })
+      })
+    );
   });
 
   it('should call transaction.send with options if passed in', () => {
     const mockOptions = { destination: 'another-queue-name' };
-    putMessageOnQueue(mockClient, MOCK_MESSAGE, mockOptions);
+    putMessageOnQueue(mockClient, mockMessage, mockOptions);
     expect(mockTransaction.send).toHaveBeenCalledTimes(1);
     expect(mockTransaction.send).toHaveBeenCalledWith(expect.objectContaining(mockOptions));
   });
 
   it('should call stream.write with message', () => {
-    putMessageOnQueue(mockClient, MOCK_MESSAGE);
+    putMessageOnQueue(mockClient, mockMessage);
     expect(mockStream.write).toHaveBeenCalledTimes(1);
-    expect(mockStream.write).toHaveBeenCalledWith(MOCK_MESSAGE);
+    expect(mockStream.write).toHaveBeenCalledWith(mockMessage);
   });
 
   it('should call stream.end', () => {
-    putMessageOnQueue(mockClient, MOCK_MESSAGE);
+    putMessageOnQueue(mockClient, mockMessage);
     expect(mockStream.end).toHaveBeenCalledTimes(1);
   });
 
   it('should call transaction.send', () => {
-    putMessageOnQueue(mockClient, MOCK_MESSAGE);
+    putMessageOnQueue(mockClient, mockMessage);
     expect(mockTransaction.commit).toHaveBeenCalledTimes(1);
   });
 });
--- src/services/queue/publisher/put-message-on-queue.js
+++ src/services/queue/publisher/put-message-on-queue.js
@@ -1,5 +1,16 @@
+import { updateLogEvent } from '../../../middleware/logging';
+
 export const putMessageOnQueue = (client, message, options) => {
+  updateLogEvent({
+    status: 'Putting Message on queue',
+    queue: {
+      options
+    }
+  });
   const transaction = client.begin();
+
+  updateLogEvent({ status: 'Sending transaction', queue: { transaction: { id: transaction.id } } });
+
   const stream = transaction.send(options);
   stream.write(message);
   stream.end();
