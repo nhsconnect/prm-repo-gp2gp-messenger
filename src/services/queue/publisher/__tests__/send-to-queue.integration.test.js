bb75008793a9539728c1a37a262b5691344b85b5
bfisher <fishey2b@gmail.com>
2020-04-02 14:55:50 +0100

    [PRMT-1083] Added tests for streaming using Readables

--- src/services/queue/publisher/__tests__/send-to-queue.integration.test.js
+++ src/services/queue/publisher/__tests__/send-to-queue.integration.test.js
@@ -1,6 +1,7 @@
 import { v4 as uuid } from 'uuid';
 import { consumeOneMessage } from '../../';
 import { sendToQueue } from '../send-to-queue';
+import { Readable } from 'stream';
 
 jest.unmock('stompit');
 jest.unmock('uuid');
@@ -15,6 +16,35 @@ describe('sendToQueue', () => {
     await sendToQueue(firstMockMessage, { destination: mockQueueName });
     const message = await consumeOneMessage({ destination: mockQueueName });
     expect(message).toEqual(firstMockMessage);
+    done();
+  });
+
+  it('should put a Readable stream onto the queue that can then be consumed', async done => {
+    const messageStream = new Readable();
+    messageStream._read = () => {};
+    messageStream.push('stream-message');
+    messageStream.push(null);
+
+    const mockQueueName = uuid();
+    await sendToQueue(messageStream, { destination: mockQueueName });
+    const message = await consumeOneMessage({ destination: mockQueueName });
+
+    expect(message).toEqual('stream-message');
+    done();
+  });
+
+  it('should put a Readable stream onto the queue from consuming message (Readable)', async done => {
+    const mockQueueName = uuid();
+    const secondQueueName = uuid();
+
+    await sendToQueue('message-to-be-streamed', { destination: mockQueueName });
+
+    await sendToQueue(await consumeOneMessage({ destination: mockQueueName }), {
+      destination: secondQueueName
+    });
+    const message = await consumeOneMessage({ destination: secondQueueName });
+
+    expect(message).toEqual('message-to-be-streamed');
     done();
   });
 
