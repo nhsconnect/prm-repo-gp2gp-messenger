02cdfe8b32076634ec09813145bcc5338d31e048
Ewrobel <ewrobel@thoughtworks.com>
2020-10-01 11:51:28 +0100

    [PRMT-1121] Add the missing quotes in the ssm parameter

--- terraform/data.tf
+++ terraform/data.tf
@@ -1,7 +1,7 @@
 data "aws_caller_identity" "current" {}
 
 data "aws_ssm_parameter" "root_zone_id" {
-  name = "/repo/prm-deductions-base-infra/output/root-zone-id
+  name = "/repo/prm-deductions-base-infra/output/root-zone-id"
 }
 
 data "aws_ssm_parameter" "private_zone_id" {
