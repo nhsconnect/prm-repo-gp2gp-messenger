43369a46702304be266940dca73a2d0c15126ea1
ewrobel-nhs <edyta.wrobel@nhs.net>
2020-11-12 18:55:42 +0000

    [PRMT-1050] Passed messageId to gp2repo when sending ehr message received notification

--- src/services/gp-to-repo/__tests__/send-ehr-message-received.test.js
+++ src/services/gp-to-repo/__tests__/send-ehr-message-received.test.js
@@ -9,25 +9,25 @@ jest.mock('../../../middleware/logging');
 
 describe('sendEhrMessageReceived', () => {
   const conversationId = uuid();
+  const messageId = uuid();
   const mockGpToRepoUrl = 'fake-url';
   const mockGpToRepoAuthKeys = 'fake-keys';
   initialiseConfig.mockReturnValue({ gpToRepoAuthKeys: 'fake-keys', gpToRepoUrl: 'fake-url' });
 
-  it('should make a PATCH request to GPToRepo with conversation ID', async done => {
+  it('should make a PATCH request to GPToRepo with conversation id and message id', async () => {
     const axiosHeaders = { headers: { Authorization: `${mockGpToRepoAuthKeys}` } };
-    await sendEhrMessageReceived(conversationId);
+    await sendEhrMessageReceived(conversationId, messageId);
     expect(axios.patch).toHaveBeenCalledTimes(1);
     expect(axios.patch).toHaveBeenCalledWith(
       `${mockGpToRepoUrl}/deduction-requests/${conversationId}/ehr-message-received`,
-      {},
+      { messageId },
       axiosHeaders
     );
-    done();
   });
 
   it('should throw 503 with axios.patch request', () => {
     axios.patch.mockRejectedValue({ response: { status: 503 } });
-    return expect(sendEhrMessageReceived(conversationId)).rejects.toThrowError(
+    return expect(sendEhrMessageReceived(conversationId, messageId)).rejects.toThrowError(
       `PATCH ${mockGpToRepoUrl}/deduction-requests/${conversationId}/ehr-message-received - Request failed`
     );
   });
