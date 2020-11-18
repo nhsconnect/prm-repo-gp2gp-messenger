412b1420dd8c814710f72399aa67c4d55f15312d
Rosie Amphlett <rosie.amphlett1@nhs.net>
2020-11-18 13:42:18 +0000

    [PRMT-1004] Updated logging to log every event

--- src/middleware/logging.js
+++ src/middleware/logging.js
@@ -5,8 +5,10 @@ import { setCorrelationInfo } from './correlation';
 
 const LOG_EVENT_KEY = 'logEvent';
 
-export const updateLogEvent = event =>
+export const updateLogEvent = event => {
   httpContext.set(LOG_EVENT_KEY, merge(httpContext.get(LOG_EVENT_KEY), event));
+  logger.info('New event info', event);
+};
 
 export const updateLogEventWithError = err =>
   updateLogEvent({ error: { ...err, message: err.message, stack: err.stack } });
