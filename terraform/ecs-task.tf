0571eaf1b4ee76683b92357bdb7d0d127bffa6e4
Rosie Amphlett <rosie.amphlett1@nhs.net>
2020-11-18 11:51:49 +0000

    [PRMT-1004] Changed protocol to https as this should be the default There is a redirect from http to https - this makes axios calls fail see example of an issue here: https://github.com/axios/axios/issues/1629

--- terraform/ecs-task.tf
+++ terraform/ecs-task.tf
@@ -11,8 +11,8 @@ locals {
       { name = "GP2GP_ADAPTOR_MHS_QUEUE_URL_1", value = data.aws_ssm_parameter.stomp-endpoint_0.value },
       { name = "GP2GP_ADAPTOR_MHS_QUEUE_URL_2", value = data.aws_ssm_parameter.stomp-endpoint_1.value },
       { name = "GP2GP_ADAPTOR_S3_BUCKET_NAME", value = var.s3_bucket_name },
-      { name = "GP2GP_ADAPTOR_EHR_REPO_URL", value = "http://${var.environment}.ehr-repo.patient-deductions.nhs.uk" },
-      { name = "GP2GP_ADAPTOR_GP_TO_REPO_URL", value = "http://${var.environment}.gp-to-repo.patient-deductions.nhs.uk" },
+      { name = "GP2GP_ADAPTOR_EHR_REPO_URL", value = "https://${var.environment}.ehr-repo.patient-deductions.nhs.uk" },
+      { name = "GP2GP_ADAPTOR_GP_TO_REPO_URL", value = "https://${var.environment}.gp-to-repo.patient-deductions.nhs.uk" },
       { name = "GP2GP_ADAPTOR_MHS_OUTBOUND_URL", value = data.aws_ssm_parameter.GP2GP_ADAPTOR_MHS_OUTBOUND_URL.value },
       { name = "GP2GP_ADAPTOR_MHS_ROUTE_URL", value = data.aws_ssm_parameter.GP2GP_ADAPTOR_MHS_ROUTE_URL.value },
     ]
