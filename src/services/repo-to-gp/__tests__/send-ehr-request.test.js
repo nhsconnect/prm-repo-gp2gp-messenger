c8cb0032fc402aefa5225083325c237196ae5e80
Rosie Amphlett <rosie.amphlett1@nhs.net>
2020-11-24 14:28:48 +0000

    [PRMT-1146] Added error handling for sendEhrRequest

--- src/services/repo-to-gp/__tests__/send-ehr-request.test.js
+++ src/services/repo-to-gp/__tests__/send-ehr-request.test.js
@@ -2,8 +2,10 @@ import { v4 as uuid } from 'uuid';
 import { initialiseConfig } from '../../../config';
 import { sendEhrRequest } from '../send-ehr-request';
 import nock from 'nock';
+import { updateLogEventWithError } from '../../../middleware/logging';
 
 jest.mock('../../../config');
+jest.mock('../../../middleware/logging');
 
 describe('sendEhrRequest', () => {
   const conversationId = uuid();
@@ -20,7 +22,27 @@ describe('sendEhrRequest', () => {
   it('should make a POST request to RepoToGP with conversation id, nhs number and ods code', async () => {
     const headers = { reqheaders: { Authorization: `${mockRepoToGpAuthKeys}` } };
     nock(mockRepoToGpUrl, headers).post(`/registration-requests/`, body).reply(201);
-    const sendReq = await sendEhrRequest(nhsNumber, conversationId, odsCode);
-    expect(sendReq).toEqual(true);
+    let error = null;
+    try {
+      await sendEhrRequest(nhsNumber, conversationId, odsCode);
+    } catch (err) {
+      error = err;
+    }
+    expect(error).toBeNull();
+  });
+
+  it('should log error when axios returns 503', async () => {
+    const headers = { reqheaders: { Authorization: `${mockRepoToGpAuthKeys}` } };
+    nock(mockRepoToGpUrl, headers).post(`/registration-requests/`, body).reply(503);
+    let error = null;
+    try {
+      await sendEhrRequest(nhsNumber, conversationId, odsCode);
+    } catch (err) {
+      error = err;
+    }
+    expect(error).not.toBeNull();
+    expect(updateLogEventWithError).toHaveBeenCalledWith(
+      'Cannot send EHR request to repo-to-gp: Request failed with status code 503'
+    );
   });
 });
--- src/services/repo-to-gp/send-ehr-request.js
+++ src/services/repo-to-gp/send-ehr-request.js
@@ -1,15 +1,17 @@
 import axios from 'axios';
 import { initialiseConfig } from '../../config';
+import { updateLogEventWithError } from '../../middleware/logging';
 
 export const sendEhrRequest = async (nhsNumber, conversationId, odsCode) => {
+  const config = initialiseConfig();
+  const url = `${config.repoToGpUrl}/registration-requests/`;
+  const body = { nhsNumber, conversationId, odsCode };
+  const headers = { headers: { Authorization: config.repoToGpAuthKeys } };
+
   try {
-    const config = initialiseConfig();
-    const url = `${config.repoToGpUrl}/registration-requests/`;
-    const body = { nhsNumber, conversationId, odsCode };
-    const headers = { headers: { Authorization: config.repoToGpAuthKeys } };
-    const response = await axios.post(url, body, headers);
-    return response.status === 201;
+    await axios.post(url, body, headers);
   } catch (err) {
-    console.log(err);
+    updateLogEventWithError(`Cannot send EHR request to repo-to-gp: ${err.message}`);
+    throw err;
   }
 };
