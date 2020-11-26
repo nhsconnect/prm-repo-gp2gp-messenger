52f6c854a7a76de80c23b86068ce39466779ed47
Mircea M <mircea.moise@gmail.com>
2020-11-26 10:45:09 +0000

    [PRMT-1303] Increased timeout to 30s see if makes a difference (previously it was defaulted to 5s)

--- test/functional/health-record-requests.test.js
+++ test/functional/health-record-requests.test.js
@@ -1,28 +1,34 @@
 import axios from 'axios';
 import adapter from 'axios/lib/adapters/http';
 
+const TIMEOUT_30_S = 30000;
+
 describe('POST /health-record-requests/:nhsNumber', () => {
-  it('should test successful POST request for /health-record-requests/:nhsNumber with Authorization', () => {
-    const nhsNumber = process.env.NHS_ENVIRONMENT === 'dev' ? '9473480032' : '9442964410';
+  it(
+    'should test successful POST request for /health-record-requests/:nhsNumber with Authorization',
+    () => {
+      const nhsNumber = process.env.NHS_ENVIRONMENT === 'dev' ? '9473480032' : '9442964410';
 
-    const body = {
-      repositoryOdsCode: 'B86041',
-      repositoryAsid: '200000001161',
-      practiceOdsCode: 'M85019',
-      conversationId: '2d8ac681-0721-4d0c-8b76-5a26987829fb'
-    };
+      const body = {
+        repositoryOdsCode: 'B86041',
+        repositoryAsid: '200000001161',
+        practiceOdsCode: 'M85019',
+        conversationId: '2d8ac681-0721-4d0c-8b76-5a26987829fb'
+      };
 
-    return expect(
-      axios.post(`${process.env.SERVICE_URL}/health-record-requests/${nhsNumber}`, body, {
-        headers: {
-          Authorization: process.env.GP2GP_ADAPTOR_AUTHORIZATION_KEYS
-        },
-        adapter
-      })
-    ).resolves.toEqual(
-      expect.objectContaining({
-        status: 204
-      })
-    );
-  });
+      return expect(
+        axios.post(`${process.env.SERVICE_URL}/health-record-requests/${nhsNumber}`, body, {
+          headers: {
+            Authorization: process.env.GP2GP_ADAPTOR_AUTHORIZATION_KEYS
+          },
+          adapter
+        })
+      ).resolves.toEqual(
+        expect.objectContaining({
+          status: 204
+        })
+      );
+    },
+    TIMEOUT_30_S
+  );
 });
--- test/functional/pds-retrieval.test.js
+++ test/functional/pds-retrieval.test.js
@@ -3,39 +3,48 @@ import adapter from 'axios/lib/adapters/http';
 import { initialiseConfig } from '../../src/config';
 import testData from '../../src/templates/__tests__/testData.json';
 
+const TIMEOUT_30_S = 30000;
 describe('End to end test of /patient-demographics/:nhsNumber', () => {
   const config = initialiseConfig();
 
-  it('should return a 200 from PDS (successful retrieval) with a valid NHS number', () => {
-    const nhsNumber =
-      process.env.NHS_ENVIRONMENT === 'dev'
-        ? testData.patient.openTest.nhsNumber
-        : testData.tppPatient.nhsNumber;
+  it(
+    'should return a 200 from PDS (successful retrieval) with a valid NHS number',
+    () => {
+      const nhsNumber =
+        process.env.NHS_ENVIRONMENT === 'dev'
+          ? testData.patient.openTest.nhsNumber
+          : testData.tppPatient.nhsNumber;
 
-    return expect(
-      axios.get(`${config.url}/patient-demographics/${nhsNumber}`, {
-        headers: {
-          Authorization: process.env.GP2GP_ADAPTOR_AUTHORIZATION_KEYS.split(',')[0]
-        },
-        adapter
-      })
-    ).resolves.toEqual(
-      expect.objectContaining({
-        status: 200
-      })
-    );
-  });
+      return expect(
+        axios.get(`${config.url}/patient-demographics/${nhsNumber}`, {
+          headers: {
+            Authorization: process.env.GP2GP_ADAPTOR_AUTHORIZATION_KEYS.split(',')[0]
+          },
+          adapter
+        })
+      ).resolves.toEqual(
+        expect.objectContaining({
+          status: 200
+        })
+      );
+    },
+    TIMEOUT_30_S
+  );
 
-  it('should return 200 from PDS if the nhs number is not exist', () => {
-    const fakeNhsNumber = '0000000000';
+  it(
+    'should return 200 from PDS if the nhs number is not exist',
+    () => {
+      const fakeNhsNumber = '0000000000';
 
-    return expect(
-      axios.get(`${config.url}/patient-demographics/${fakeNhsNumber}`, {
-        headers: {
-          Authorization: process.env.GP2GP_ADAPTOR_AUTHORIZATION_KEYS
-        },
-        adapter
-      })
-    ).rejects.toEqual(Error('Request failed with status code 503'));
-  });
+      return expect(
+        axios.get(`${config.url}/patient-demographics/${fakeNhsNumber}`, {
+          headers: {
+            Authorization: process.env.GP2GP_ADAPTOR_AUTHORIZATION_KEYS
+          },
+          adapter
+        })
+      ).rejects.toEqual(Error('Request failed with status code 503'));
+    },
+    TIMEOUT_30_S
+  );
 });
