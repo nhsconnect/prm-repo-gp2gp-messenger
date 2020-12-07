bc2824c2706a5413005186f9870a3605a7cb2a17
Tomasz Setkowski <tomasz.setkowski2@nhs.net>
2020-12-07 15:53:22 +0000

    PRMT-1407 Added PDS update e2e test

--- test/config.js
+++ test/config.js
@@ -1,4 +1,5 @@
 export const config = {
+  nhsEnvironment: process.env.NHS_ENVIRONMENT,
   gp2gpAdaptorUrl: process.env.SERVICE_URL, // FIXME: rename to add prefix
   gp2gpAdaptorAuthorizationKeys: process.env.GP2GP_ADAPTOR_AUTHORIZATION_KEYS
 };
---
+++ test/e2e/pds-update.test.js
@@ -0,0 +1,110 @@
+import axios from 'axios';
+import adapter from 'axios/lib/adapters/http';
+import { config } from '../config';
+
+describe('Patient ODS code update in PDS', () => {
+  const RETRY_COUNT = 20;
+  const POLLING_INTERVAL_MS = 500;
+  const TEST_TIMEOUT = 3 * RETRY_COUNT * POLLING_INTERVAL_MS;
+
+  const testData = {
+    dev: {},
+    test: {
+      odsCode1: 'A20047',
+      odsCode2: 'B86041',
+      nhsNumber: 9692295990
+    }
+  };
+
+  it(
+    'should update ODS code of the patient',
+    async () => {
+      const { nhsNumber, odsCode1, odsCode2 } = testData[config.nhsEnvironment];
+      const {
+        odsCode: oldOdsCode,
+        patientPdsId,
+        serialChangeNumber,
+        conversationId
+      } = await getAndValidatePatientPdsDetails(nhsNumber);
+
+      let newOdsCode;
+      if (oldOdsCode === odsCode1) {
+        newOdsCode = odsCode2;
+      } else if (oldOdsCode === odsCode2) {
+        newOdsCode = odsCode1;
+      } else {
+        expect(
+          true,
+          'Patient allocated to automated tests is assigned to unexpected ODS code'
+        ).toBe(false);
+      }
+
+      await updateAndValidatePatientOdsCode(
+        nhsNumber,
+        patientPdsId,
+        serialChangeNumber,
+        newOdsCode,
+        conversationId
+      );
+
+      // poll until ODS is as expected
+      let patientOdsCode;
+      for (let i = 0; i < RETRY_COUNT; i++) {
+        const pdsDetails = await getAndValidatePatientPdsDetails(nhsNumber);
+        patientOdsCode = pdsDetails.odsCode;
+        await sleep(POLLING_INTERVAL_MS);
+        if (patientOdsCode === newOdsCode) {
+          break;
+        }
+      }
+
+      expect(patientOdsCode).toBe(newOdsCode);
+    },
+    TEST_TIMEOUT
+  );
+});
+
+const sleep = async ms => {
+  return new Promise(resolve => setTimeout(resolve, ms));
+};
+
+const getAndValidatePatientPdsDetails = async nhsNumber => {
+  const pdsResponse = await axios.get(
+    `${config.gp2gpAdaptorUrl}/patient-demographics/${nhsNumber}`,
+    {
+      headers: {
+        Authorization: config.gp2gpAdaptorAuthorizationKeys
+      },
+      adapter
+    }
+  );
+  expect(pdsResponse.status).toBe(200);
+
+  return { ...pdsResponse.data.data, conversationId: pdsResponse.data.conversationId };
+};
+
+const updateAndValidatePatientOdsCode = async (
+  nhsNumber,
+  pdsId,
+  serialChangeNumber,
+  newOdsCode,
+  conversationId
+) => {
+  const pdsResponse = await axios.patch(
+    `${config.gp2gpAdaptorUrl}/patient-demographics/${nhsNumber}`,
+    {
+      pdsId,
+      serialChangeNumber,
+      newOdsCode,
+      conversationId
+    },
+    {
+      headers: {
+        Authorization: config.gp2gpAdaptorAuthorizationKeys
+      },
+      adapter
+    }
+  );
+
+  expect(pdsResponse.status).toBe(204);
+};
