21896f87f2989ff333238df70fd591efbfb3ab0e
ewrobel-nhs <edyta.wrobel@nhs.net>
2020-12-16 10:28:20 +0000

    [PRMT-1471] Fixed low dependency vulnerability and updated syntax to ES6

--- src/server.js
+++ src/server.js
@@ -7,16 +7,16 @@ const config = initialiseConfig();
 const gp2gpWorker = config.enableWorker;
 const gp2gpServer = config.enableServer;
 
-function sleep(ms) {
+const sleep = ms => {
   return new Promise(resolve => setTimeout(resolve, ms));
-}
+};
 
-async function wait() {
+const wait = async () => {
   // eslint-disable-next-line no-constant-condition
   while (true) {
     await sleep(2000);
   }
-}
+};
 
 if (gp2gpWorker === 'true') {
   initialiseSubscriber();
