bc851d37c8a3f3ec5ea240e581701ca1193e83e3
Mircea M <mircea.moise@gmail.com>
2020-12-22 17:29:55 +0000

    [PRMT-1162] Moved tests from app.integration to app.nock.integration to increase the scope of testing to include axios rather than mocking it

--- src/__tests__/app.nock.integration.test.js
+++ src/__tests__/app.nock.integration.test.js
@@ -4,14 +4,22 @@ import request from 'supertest';
 import app from '../app';
 //TODO - extract it somewhere else
 import { templateEhrExtract } from '../services/parser/message/__tests__/update-extract-for-sending.test';
+import { pdsRetrivealQueryResponseSuccess } from '../services/pds/__tests__/data/pds-retrieval-query-response-success';
 
 const authKey = 'correct-key';
+const host = 'http://localhost';
+const odsCode = 'B1234';
+
+beforeAll(() => {
+  process.env.GP2GP_ADAPTOR_AUTHORIZATION_KEYS = authKey;
+  process.env.GP2GP_ADAPTOR_MHS_OUTBOUND_URL = 'http://localhost/mhs-outbound';
+  process.env.GP2GP_ADAPTOR_MHS_ROUTE_URL = 'http://localhost/mhs-route';
+});
+
 describe('POST /health-record-transfers', () => {
-  const host = 'http://localhost';
   const ehrPath = '/conversationId/messageId';
   const currentEhrUrl = `${host}${ehrPath}`;
   const conversationId = '41291044-8259-4D83-AE2B-93B7BFCABE73';
-  const odsCode = 'B1234';
   const priorEhrRequestId = uuid();
   const expectedReceivingAsid = '200000000678';
   const ehrRequestId = '26A541CE-A5AB-4713-99A4-150EC3DA25C6';
@@ -28,11 +36,6 @@ describe('POST /health-record-transfers', () => {
       }
     }
   };
-  beforeEach(() => {
-    process.env.GP2GP_ADAPTOR_AUTHORIZATION_KEYS = authKey;
-    process.env.GP2GP_ADAPTOR_MHS_OUTBOUND_URL = 'http://localhost/mhs-outbound';
-    process.env.GP2GP_ADAPTOR_MHS_ROUTE_URL = 'http://localhost/mhs-route';
-  });
 
   const ehrExtractStoredInS3 = ehrExtract => `----=_MIME-Boundary
 <SOAP:Header/>
@@ -78,6 +81,79 @@ ${ehrExtract}
         expect(ehrRepoScope.isDone()).toBe(true);
         expect(mhsOutboundScope.isDone()).toBe(true);
         expect(mhsRouteScope.isDone()).toBe(true);
+      })
+      .end(done);
+  });
+});
+
+describe('GET /patient-demographics', () => {
+  it('should return a 200 status code for /patient-demographics/:nhsNumber', done => {
+    const mhsOutboundHeaders = {
+      'Content-Type': 'application/json',
+      'Interaction-ID': 'QUPA_IN000008UK02',
+      'Sync-Async': false,
+      'Ods-Code': 'YES',
+      'from-asid': '200000001161'
+    };
+
+    const mhsOutboundScope = nock(host, { reqheaders: mhsOutboundHeaders })
+      .post('/mhs-outbound')
+      .reply(200, pdsRetrivealQueryResponseSuccess);
+
+    request(app)
+      .get('/patient-demographics/9999999999')
+      .set('Authorization', authKey)
+      .expect(200)
+      .expect(res => {
+        expect(mhsOutboundScope.isDone()).toBe(true);
+        expect(res.body).toEqual(
+          expect.objectContaining({
+            data: {
+              odsCode: 'B86041',
+              patientPdsId: 'cppz',
+              serialChangeNumber: '138'
+            }
+          })
+        );
+      })
+      .end(done);
+  });
+});
+
+describe('POST /health-record-requests/:nhsNumber/acknowledgement', () => {
+  const practiceAsid = '200007389';
+  const conversationId = uuid();
+  const mockBody = {
+    conversationId,
+    messageId: uuid(),
+    odsCode: odsCode,
+    repositoryAsid: '20000018274'
+  };
+
+  it('should successfully send acknowledgement and return 204', done => {
+    const mhsOutboundHeaders = {
+      'Content-Type': 'application/json',
+      'Interaction-ID': 'MCCI_IN010000UK13',
+      'Correlation-ID': conversationId,
+      'Sync-Async': false,
+      'Ods-Code': odsCode,
+      'from-asid': '200000001161'
+    };
+
+    const mhsRouteScope = nock(`${host}/mhs-route`)
+      .get(`/routing?org-code=${odsCode}&service-id=urn:nhs:names:services:gp2gp:MCCI_IN010000UK13`)
+      .reply(200, { uniqueIdentifier: [practiceAsid] });
+
+    const mhsOutboundScope = nock(host, mhsOutboundHeaders).post('/mhs-outbound').reply(204);
+
+    request(app)
+      .post('/health-record-requests/9999999999/acknowledgement')
+      .set('Authorization', authKey)
+      .send(mockBody)
+      .expect(204)
+      .expect(() => {
+        expect(mhsRouteScope.isDone()).toBe(true);
+        expect(mhsOutboundScope.isDone()).toBe(true);
       })
       .end(done);
   });
