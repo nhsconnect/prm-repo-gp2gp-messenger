00b705dee790d841c56b8c113878051fb9739cf4
aiwatko <oktawia.kata1@nhs.net>
2021-03-19 11:57:39 +0000

    [PRMT-1556] Added tests for structure logging containing conversationId, messageId and traceId

--- src/__tests__/app.integration.test.js
+++ src/__tests__/app.integration.test.js
@@ -5,8 +5,9 @@ import app from '../app';
 import { pdsQueryActFailed } from '../services/pds/__tests__/data/pds-query-act-failed';
 import { pdsRetrivealQueryResponseSuccess } from '../services/pds/__tests__/data/pds-retrieval-query-response-success';
 import { getPracticeAsid } from '../services/mhs/mhs-route-client';
+import { expectStructuredLogToContain, transportSpy } from '../__builders__/logging-helper';
+import { logger } from '../config/logging';
 
-jest.mock('../middleware/logging');
 jest.mock('axios');
 jest.mock('../services/mhs/mhs-route-client');
 
@@ -52,58 +53,87 @@ describe('app', () => {
   });
 
   describe('GET /patient-demographics', () => {
-    it('should return a 200 status code for /patient-demographics/:nhsNumber', done => {
-      axios.post.mockImplementation(() =>
-        Promise.resolve({ status: 200, data: pdsRetrivealQueryResponseSuccess })
-      );
-      request(app)
-        .get('/patient-demographics/9999999999')
-        .set('Authorization', 'correct-key')
-        .expect(200)
-        .end(done);
+    beforeEach(() => {
+      logger.add(transportSpy);
     });
 
-    it('should return the response body for /patient-demographics/:nhsNumber', done => {
+    it('should return a 200 status code for /patient-demographics/:nhsNumber', async () => {
       axios.post.mockImplementation(() =>
         Promise.resolve({ status: 200, data: pdsRetrivealQueryResponseSuccess })
       );
-      request(app)
+
+      const res = await request(app)
         .get('/patient-demographics/9999999999')
-        .set('Authorization', 'correct-key')
-        .expect(200)
-        .expect(res => {
-          expect(res.body).toEqual(
-            expect.objectContaining({
-              data: {
-                odsCode: 'B86041',
-                patientPdsId: 'cppz',
-                serialChangeNumber: '138'
-              }
-            })
-          );
+        .set('Authorization', 'correct-key');
+
+      expect(res.statusCode).toBe(200);
+      expectStructuredLogToContain(transportSpy, {
+        conversationId: expect.anything(),
+        traceId: expect.anything()
+      });
+    });
+
+    it('should return the response body for /patient-demographics/:nhsNumber', async () => {
+      axios.post.mockImplementation(() =>
+        Promise.resolve({ status: 200, data: pdsRetrivealQueryResponseSuccess })
+      );
+      const res = await request(app)
+        .get('/patient-demographics/9999999999')
+        .set('Authorization', 'correct-key');
+
+      expect(res.statusCode).toBe(200);
+      expect(res.body).toEqual(
+        expect.objectContaining({
+          data: {
+            odsCode: 'B86041',
+            patientPdsId: 'cppz',
+            serialChangeNumber: '138'
+          }
         })
-        .end(done);
+      );
+      expectStructuredLogToContain(transportSpy, {
+        conversationId: expect.anything(),
+        traceId: expect.anything()
+      });
     });
 
-    it('should return a 503 status code with Errors for /patient-demographics/:nhsNumber', done => {
+    it('should return a 503 status code with Errors for /patient-demographics/:nhsNumber', async () => {
       axios.post.mockImplementation(() =>
         Promise.resolve({ status: 200, data: pdsQueryActFailed() })
       );
-      request(app)
+      const res = await request(app)
         .get('/patient-demographics/9999999999')
-        .set('Authorization', 'correct-key')
-        .expect(503)
-        .end(done);
+        .set('Authorization', 'correct-key');
+
+      expect(res.statusCode).toBe(503);
+      expectStructuredLogToContain(transportSpy, {
+        conversationId: expect.anything(),
+        traceId: expect.anything()
+      });
     });
 
-    it('should return a 404 status code without nhsNumber parameter', done => {
-      request(app).get('/patient-demographics').expect(404).end(done);
+    it('should return a 404 status code without nhsNumber parameter', async () => {
+      const res = await request(app).get('/patient-demographics');
+
+      expect(res.statusCode).toBe(404);
+      expectStructuredLogToContain(transportSpy, {
+        traceId: expect.anything()
+      });
     });
   });
 
   describe('POST /health-record-requests/:nhsNumber', () => {
-    it('should return a 401 status code for /health-record-requests/:nhsNumber when not authenticated', done => {
-      request(app).post('/health-record-requests/9999999999').expect(401).end(done);
+    beforeEach(() => {
+      logger.add(transportSpy);
+    });
+
+    it('should return a 401 status code for /health-record-requests/:nhsNumber when not authenticated', async () => {
+      const res = await request(app).post('/health-record-requests/9999999999');
+
+      expect(res.statusCode).toBe(401);
+      expectStructuredLogToContain(transportSpy, {
+        traceId: expect.anything()
+      });
     });
   });
 
@@ -116,28 +146,42 @@ describe('app', () => {
       repositoryAsid: '20000018274'
     };
 
-    it('should successfully send acknowledgement and return 204', done => {
+    beforeEach(() => {
+      logger.add(transportSpy);
+    });
+
+    it('should successfully send acknowledgement and return 204', async () => {
       getPracticeAsid.mockResolvedValue(practiceAsid);
       axios.post.mockResolvedValue({ status: 204 });
 
-      request(app)
+      const res = await request(app)
         .post('/health-record-requests/9999999999/acknowledgement')
         .set('Authorization', 'correct-key')
-        .send(mockBody)
-        .expect(204)
-        .end(done);
+        .send(mockBody);
+
+      expect(res.statusCode).toBe(204);
+      expectStructuredLogToContain(transportSpy, {
+        conversationId: expect.anything(),
+        messageId: expect.anything(),
+        traceId: expect.anything()
+      });
     });
 
-    it('should error when send message fails', done => {
+    it('should error when send message fails', async () => {
       getPracticeAsid.mockResolvedValue(practiceAsid);
       axios.post.mockRejectedValue({ status: 500 });
 
-      request(app)
+      const res = await request(app)
         .post(`/health-record-requests/9999999999/acknowledgement`)
         .set('Authorization', 'correct-key')
-        .send(mockBody)
-        .expect(503)
-        .end(done);
+        .send(mockBody);
+
+      expect(res.statusCode).toBe(503);
+      expectStructuredLogToContain(transportSpy, {
+        conversationId: expect.anything(),
+        messageId: expect.anything(),
+        traceId: expect.anything()
+      });
     });
   });
 });
