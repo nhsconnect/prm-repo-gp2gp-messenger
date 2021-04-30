8cf257ec7c2a56677f13714732c8bf03eed50e96
aiwatko <oktawia.kata1@nhs.net>
2021-04-30 11:33:33 +0100

    [PRMT-1990] Updated the service url to fit the new pattern

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "service_url" {
   name  = "/repo/${var.environment}/output/${var.repo_name}/service-url"
   type  = "String"
-  value = "https://${var.environment}.${var.dns_name}.patient-deductions.nhs.uk"
+  value = "https://${var.dns_name}.${var.environment}.non-prod.patient-deductions.nhs.uk"
 }
--- terraform/route53.tf
+++ terraform/route53.tf
@@ -1,19 +1,14 @@
 locals {
-  # deductions_private_alb_dns = data.aws_ssm_parameter.deductions_private_alb_dns.value
    deductions_private_internal_alb_dns = data.aws_ssm_parameter.deductions_private_alb_internal_dns.value
 }
 
-# resource "aws_route53_record" "gp2gp-r53-record" {
-#   zone_id = data.aws_ssm_parameter.root_zone_id.value
-#   name    = "${var.environment}.${var.dns_name}"
-#   type    = "CNAME"
-#   ttl     = "300"
-#   records = [local.deductions_private_alb_dns]
-# }
+data "aws_ssm_parameter" "environment_private_zone_id" {
+  name = "/repo/${var.environment}/output/prm-deductions-infra/environment-private-zone-id"
+}
 
-resource "aws_route53_record" "gp2gp-r53-private-record" {
-  zone_id = data.aws_ssm_parameter.private_zone_id.value
-  name    = "${var.environment}.${var.dns_name}"
+resource "aws_route53_record" "gp2gp-adaptor-private-record" {
+  zone_id = data.aws_ssm_parameter.environment_private_zone_id.value
+  name    = var.dns_name
   type    = "CNAME"
   ttl     = "300"
   records = [local.deductions_private_internal_alb_dns]
