6ff777bf20e401df3a29b0623be71941ffc3ef91
Rosie Amphlett <rosie.amphlett1@nhs.net>
2021-06-11 10:53:05 +0100

    [Merge conflict] Removed root zone id value from data

--- terraform/data.tf
+++ terraform/data.tf
@@ -1,9 +1,5 @@
 data "aws_caller_identity" "current" {}
 
-data "aws_ssm_parameter" "root_zone_id" {
-  name = "/repo/output/prm-deductions-base-infra/root-zone-id"
-}
-
 data "aws_ssm_parameter" "private_zone_id" {
   name = "/repo/${var.environment}/output/prm-deductions-infra/private-root-zone-id"
 }
