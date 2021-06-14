fae386b6fd99a27a2fa121d1e24b5cf5a5898951
Rosie Amphlett <rosie.amphlett1@nhs.net>
2021-06-14 17:51:33 +0100

    [PRMT-2046] Refactored auth tests to use async await

--- src/middleware/__tests__/auth.test.js
+++ src/middleware/__tests__/auth.test.js
@@ -5,166 +5,98 @@ import { initializeConfig } from '../../config';
 import { logInfo, logWarning } from '../logging';
 
 jest.mock('../logging');
-jest.mock('../../config');
 jest.mock('../../api/patient-demographics/pds-retrieval');
+jest.mock('../../config', () => ({
+  initializeConfig: jest.fn().mockReturnValue({
+    consumerApiKeys: {
+      TEST_USER: 'correct-key',
+      DUPLICATE_TEST_USER: 'correct-key',
+      USER_2: 'key_2'
+    }
+  })
+}));
 
 // In all other unit tests we want to pass through all of this logic and should therefore call jest.mock
 // jest.mock('../auth') will call the manual mock in __mocks__ automatically
 describe('auth', () => {
-  beforeEach(() => {
-    pdsRetrieval.mockImplementation((req, res) => {
-      res.sendStatus(201);
-    });
-    initializeConfig.mockReturnValue({
-      consumerApiKeys: {
-        TEST_USER: 'correct-key',
-        DUPLICATE_TEST_USER: 'correct-key',
-        USER_2: 'key_2'
-      }
-    });
-  });
-
   describe('authenticated successfully', () => {
-    it('should return HTTP 201 when correctly authenticated', done => {
-      request(app)
+    it('should return HTTP 201 when correctly authenticated', async () => {
+      pdsRetrieval.mockImplementation((req, res) => res.sendStatus(201));
+      const res = await request(app)
         .get('/patient-demographics/0000000000')
-        .set('Authorization', 'correct-key')
-        .expect(201)
-        .end(done);
+        .set('Authorization', 'correct-key');
+
+      expect(res.statusCode).toBe(201);
     });
   });
 
   describe('consumerApiKeys environment variables not provided', () => {
-    it('should return 412 if consumerApiKeys have not been set', done => {
+    it('should return 412 with an explicit error message if consumerApiKeys have not been set', async () => {
       initializeConfig.mockReturnValueOnce({ consumerApiKeys: {} });
-      request(app)
-        .get('/patient-demographics/0000000000')
-        .set('Authorization', 'correct-key')
-        .expect(412)
-        .end(done);
-    });
+      const errorMessage = {
+        error: 'Server-side Authorization keys have not been set, cannot authenticate'
+      };
 
-    it('should return an explicit error message in the body if consumerApiKeys have not been set', done => {
-      initializeConfig.mockReturnValueOnce({ consumerApiKeys: {} });
-      request(app)
+      const res = await request(app)
         .get('/patient-demographics/0000000000')
-        .set('Authorization', 'correct-key')
-        .expect(res => {
-          expect(res.body).toEqual(
-            expect.objectContaining({
-              error: 'Server-side Authorization keys have not been set, cannot authenticate'
-            })
-          );
-        })
-        .end(done);
+        .set('Authorization', 'correct-key');
+
+      expect(res.statusCode).toBe(412);
+      expect(res.body).toEqual(errorMessage);
     });
   });
 
   describe('Authorization header not provided', () => {
-    it('should return HTTP 401 when no authorization header provided', done => {
-      request(app).get('/patient-demographics/0000000000').expect(401).end(done);
-    });
+    it('should return HTTP 401 with an explicit error message when no authorization header provided', async () => {
+      const errorMessage = {
+        error: 'The request (/patient-demographics) requires a valid Authorization header to be set'
+      };
+      const res = await request(app).get('/patient-demographics/0000000000');
 
-    it('should return an explicit error message in the body when no authorization header provided', done => {
-      request(app)
-        .get('/patient-demographics/0000000000')
-        .expect(res => {
-          expect(res.body).toEqual(
-            expect.objectContaining({
-              error:
-                'The request (/patient-demographics) requires a valid Authorization header to be set'
-            })
-          );
-        })
-        .end(done);
+      expect(res.statusCode).toBe(401);
+      expect(res.body).toEqual(errorMessage);
     });
   });
 
   describe('Incorrect Authorisation header value provided ', () => {
-    it('should return HTTP 403 when authorization key is incorrect', done => {
-      request(app)
+    it('should return HTTP 403 with an explicit error message when authorization key is incorrect', async () => {
+      const errorMessage = { error: 'Authorization header is provided but not valid' };
+      const res = await request(app)
         .get('/patient-demographics/0000000000')
-        .set('Authorization', 'incorrect-key')
-        .expect(403)
-        .end(done);
-    });
+        .set('Authorization', 'incorrect-key');
 
-    it('should return an explicit error message in the body when authorization key is incorrect', done => {
-      request(app)
-        .get('/patient-demographics/0000000000')
-        .set('Authorization', 'incorrect-key')
-        .expect(res => {
-          expect(res.body).toEqual(
-            expect.objectContaining({
-              error: 'Authorization header is provided but not valid'
-            })
-          );
-        })
-        .end(done);
-    });
-  });
-
-  describe('should only authenticate with exact value of the auth key', () => {
-    it('should return HTTP 403 when authorization key is partial string', done => {
-      initializeConfig.mockReturnValueOnce({
-        consumerApiKeys: { TEST_USER: 'correct-key,other-key' }
-      });
-      request(app)
-        .get('/patient-demographics/0000000000')
-        .set('Authorization', 'correct-key')
-        .expect(403)
-        .end(done);
-    });
-
-    it('should return HTTP 201 when authorization keys have a comma but are one string ', done => {
-      initializeConfig.mockReturnValueOnce({
-        consumerApiKeys: { TEST_USER: 'correct-key,other-key' }
-      });
-      request(app)
-        .get('/patient-demographics/0000000000')
-        .set('Authorization', 'correct-key,other-key')
-        .expect(201)
-        .end(done);
+      expect(res.statusCode).toBe(403);
+      expect(res.body).toEqual(errorMessage);
     });
   });
 
   describe('Auth logging', () => {
-    it('should log consumer, method and url for correctly authenticated request', done => {
-      request(app)
-        .get('/patient-demographics/0000000000')
-        .set('Authorization', 'key_2')
-        .expect(() => {
-          expect(logInfo).toHaveBeenCalledWith(
-            'Consumer: USER_2, Request: GET /patient-demographics/0000000000'
-          );
-        })
-        .end(done);
+    it('should log consumer, method and url for correctly authenticated request', async () => {
+      const logMessage = 'Consumer: USER_2, Request: GET /patient-demographics/0000000000';
+      await request(app).get('/patient-demographics/0000000000').set('Authorization', 'key_2');
+
+      expect(logInfo).toHaveBeenCalledWith(logMessage);
     });
 
-    it('should log multiple consumers when they use the same key value', done => {
-      request(app)
+    it('should log multiple consumers when they use the same key value', async () => {
+      const logMessage =
+        'Consumer: TEST_USER/DUPLICATE_TEST_USER, Request: GET /patient-demographics/0000000000';
+      await request(app)
         .get('/patient-demographics/0000000000')
-        .set('Authorization', 'correct-key')
-        .expect(() => {
-          expect(logInfo).toHaveBeenCalledWith(
-            'Consumer: TEST_USER/DUPLICATE_TEST_USER, Request: GET /patient-demographics/0000000000'
-          );
-        })
-        .end(done);
+        .set('Authorization', 'correct-key');
+
+      expect(logInfo).toHaveBeenCalledWith(logMessage);
     });
 
-    it('should log the method, url and partial api key when a request is unsuccessful', done => {
-      request(app)
+    it('should log the method, url and partial api key when a request is unsuccessful', async () => {
+      const logMessage =
+        'Unsuccessful Request: GET /patient-demographics/0000000000, API Key: ******key';
+      await request(app)
         .get('/patient-demographics/0000000000')
         .send({ nhsNumber: '0000000000' })
-        .set('Authorization', 'incorrect-key')
-        .expect(403)
-        .expect(() => {
-          expect(logWarning).toHaveBeenCalledWith(
-            'Unsuccessful Request: GET /patient-demographics/0000000000, API Key: ******key'
-          );
-        })
-        .end(done);
+        .set('Authorization', 'incorrect-key');
+
+      expect(logWarning).toHaveBeenCalledWith(logMessage);
     });
   });
 });
