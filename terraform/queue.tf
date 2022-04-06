bd8b9998c9e5d4cd5c4ea9f5690517de52ce8cc7
Mario Giampietri <mario.giampietri1@nhs.net>
2022-04-06 12:23:32 +0100

    [PRMT-2568] added observability queue with kms key

---
+++ terraform/queue.tf
@@ -0,0 +1,74 @@
+locals {
+  hl7-message-sent-observability-queue-name = "${var.environment}-hl7-message-sent-observability"
+}
+
+resource "aws_sqs_queue" "hl7_message_sent_observability" {
+  name                       = local.hl7-message-sent-observability-queue-name
+  message_retention_seconds  = 1209600
+  kms_master_key_id = aws_kms_key.hl7_message_sent_observability.key_id
+  receive_wait_time_seconds = 20
+  visibility_timeout_seconds = 240
+
+  tags = {
+    Name = local.hl7-message-sent-observability-queue-name
+    CreatedBy   = var.repo_name
+    Environment = var.environment
+  }
+}
+
+resource "aws_kms_key" "hl7_message_sent_observability" {
+  description = "Custom KMS Key to enable server side encryption for SQS"
+  policy      = data.aws_iam_policy_document.kms_key_policy_doc.json
+  enable_key_rotation = true
+
+  tags = {
+    Name        = "${local.hl7-message-sent-observability-queue-name}-kms-key"
+    CreatedBy   = var.repo_name
+    Environment = var.environment
+  }
+}
+
+data "aws_iam_policy_document" "kms_key_policy_doc" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+      type        = "AWS"
+    }
+    actions   = ["kms:*"]
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+
+    principals {
+      identifiers = ["sns.amazonaws.com"]
+      type        = "Service"
+    }
+
+    actions = [
+      "kms:Decrypt",
+      "kms:GenerateDataKey*"
+    ]
+
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+
+    principals {
+      identifiers = ["cloudwatch.amazonaws.com"]
+      type        = "Service"
+    }
+
+    actions = [
+      "kms:Decrypt",
+      "kms:GenerateDataKey*"
+    ]
+
+    resources = ["*"]
+  }
+}
