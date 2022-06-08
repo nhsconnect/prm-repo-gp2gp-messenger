640cf38731331e7931ba54b3f6302bd1f892c263
Jack Slater <jack.slater1@nhs.net>
2022-06-08 12:24:37 +0100

    [TECH] add logs to show effectiveDate from pds retrieval resoponse

--- src/services/parser/pds/extract-ods-code.js
+++ src/services/parser/pds/extract-ods-code.js
@@ -1,14 +1,23 @@
 import { XmlParser } from '../xml-parser/xml-parser';
+import { logInfo } from '../../../middleware/logging';
 
 export const extractOdsCode = message =>
   new XmlParser()
     .parse(message)
     .then(jsObject => jsObject.findAll('patientCareProvisionEvent'))
-    .then(
-      jsObjectArray =>
-        jsObjectArray.find(jsObject => jsObject.code.code === '1').performer.assignedEntity.id
-          .extension
-    )
+    .then(patientCareProvisionEvents => {
+      logInfo('Total number of patientCareProvisionEvents: ' + patientCareProvisionEvents.length);
+      const effectiveTime = patientCareProvisionEvents.find('effectiveTime');
+      const odsCode = patientCareProvisionEvents.find(jsObject => jsObject.code.code === '1')
+        .performer.assignedEntity.id.extension;
+      logInfo(
+        'Last patientCareProvisionEvent ods code: ' +
+          odsCode +
+          '. EffectiveTime: ' +
+          JSON.stringify(effectiveTime)
+      );
+      return odsCode;
+    })
     .catch(() => {
       throw Error('Failed to extract ODS Code');
     });
