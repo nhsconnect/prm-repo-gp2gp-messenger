6f97d048dfe9bb8e0d11eff34403d7eb9a6f5e1e
Shruthi Chandekar <shruthi.chandekar1@nhs.net>
2022-06-20 22:59:53 +0100

    [PRMT-1565] Added CloudWatch metrics 'ErrorCountInLogs' for GP2GPMessenger service.

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -1,8 +1,26 @@
+locals {
+  error_logs_metric_name              = "ErrorCountInLogs"
+  gp2gp_messenger_service_metric_namespace = "GP2GPMessengerService"
+}
+
 resource "aws_cloudwatch_log_group" "log_group" {
   name = "/nhs/deductions/${var.environment}-${data.aws_caller_identity.current.account_id}/${var.component_name}"
 
   tags = {
     Environment = var.environment
     CreatedBy = var.repo_name
+  }
+}
+
+resource "aws_cloudwatch_log_metric_filter" "log_metric_filter" {
+  name           = "${var.environment}-${var.component_name}-error-logs"
+  pattern        = "{ $.level = \"ERROR\" }"
+  log_group_name = aws_cloudwatch_log_group.log_group.name
+
+  metric_transformation {
+    name          = local.error_logs_metric_name
+    namespace     = local.gp2gp_messenger_service_metric_namespace
+    value         = 1
+    default_value = 0
   }
 }
