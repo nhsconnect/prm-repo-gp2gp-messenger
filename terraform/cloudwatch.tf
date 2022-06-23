9c3018d9df2628c9a854e5b4a1ecbc96226c640e
Shruthi Chandekar <shruthi.chandekar1@nhs.net>
2022-06-23 15:57:07 +0100

    [PRMT-1565] Added ssm parameter for gp2gpmessenger namespace.
    Co-authored-by: eyupaksu-nhs <eyup.aksu@thoughtworks.com>
    Co-authored-by: romanenhs <romane.castera1@nhs.net>

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -1,6 +1,6 @@
 locals {
   error_logs_metric_name              = "ErrorCountInLogs"
-  gp2gp_messenger_service_metric_namespace = "gp2gp-messenger"
+  gp2gp_messenger_service_metric_namespace = "Gp2GpMessenger"
 }
 
 resource "aws_cloudwatch_log_group" "log_group" {
@@ -22,5 +22,15 @@ resource "aws_cloudwatch_log_metric_filter" "log_metric_filter" {
     namespace     = local.gp2gp_messenger_service_metric_namespace
     value         = 1
     default_value = 0
+  }
+}
+
+resource "aws_ssm_parameter" "error_log_metric_namespace_name" {
+  name = "/repo/${var.environment}/output/${var.repo_name}/error_log_metric_namespace"
+  type = "String"
+  value = local.gp2gp_messenger_service_metric_namespace
+  tags = {
+    CreatedBy   = var.repo_name
+    Environment = var.environment
   }
 }
