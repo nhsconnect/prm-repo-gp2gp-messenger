baa683a4e1eb40c237d9d978279da2dff4069541
Dan Moore <dan.moore1@nhs.net>
2022-09-28 15:40:03 +0100

    [PRMT-2865] Moved stuff around

---
+++ src/api/health-record-transfers/fragment.js
@@ -0,0 +1,36 @@
+import { retrieveMessageFromRepo } from '../../services/ehr/retrieve-message-from-repo';
+import { getPracticeAsid } from '../../services/fhir/sds-fhir-client';
+import { sendMessage } from '../../services/mhs/mhs-outbound-client';
+import { updateFragmentForSending } from '../../services/parser/message/update-fragment-for-sending';
+
+export const healthRecordTransfersFragment = async (req, res) => {
+  const COPC_INTERACTION_ID = 'COPC_IN000001UK01';
+  const serviceId = `urn:nhs:names:services:gp2gp:${COPC_INTERACTION_ID}`;
+  const { data } = req.body;
+  const {
+    outboundMessageId: messageId,
+    outboundConversationId: conversationId,
+    attributes: { recipientOdsCode },
+    links: { originalMessageUrl }
+  } = data;
+
+  const mhsJsonFragment = await retrieveMessageFromRepo(originalMessageUrl);
+  const recipientAsid = await getPracticeAsid(recipientOdsCode, serviceId);
+  console.log('just for lint: ' + mhsJsonFragment + recipientAsid);
+  await sendMessage({
+    interactionId: COPC_INTERACTION_ID,
+    conversationId,
+    odsCode: recipientOdsCode,
+    // WIP stubbed
+    message: updateFragmentForSending(
+      COPC_INTERACTION_ID,
+      recipientAsid,
+      conversationId,
+      messageId
+    ),
+    attachments: [],
+    external_attachments: []
+  });
+
+  res.sendStatus(204);
+};
--- src/api/health-record-transfers/index.js
+++ src/api/health-record-transfers/index.js
@@ -2,9 +2,7 @@ import express from 'express';
 import { authenticateRequest } from '../../middleware/auth';
 import { validate } from '../../middleware/validation';
 import { healthRecordTransfers, healthRecordTransferValidation } from './health-record-transfers';
-import { retrieveMessageFromRepo } from '../../services/ehr/retrieve-message-from-repo';
-import { sendMessage } from '../../services/mhs/mhs-outbound-client';
-import { getPracticeAsid } from '../../services/fhir/sds-fhir-client';
+import { healthRecordTransfersFragment } from './fragment';
 
 export const healthRecordTransferRouter = express.Router();
 
@@ -15,32 +13,6 @@ healthRecordTransferRouter.post(
   validate,
   healthRecordTransfers
 );
-
-const healthRecordTransfersFragment = async (req, res) => {
-  const COPC_INTERACTION_ID = 'COPC_IN000001UK01';
-  const serviceId = `urn:nhs:names:services:gp2gp:${COPC_INTERACTION_ID}`;
-  const { data } = req.body;
-  const {
-    outboundConversationId: conversationId,
-    attributes: { recipientOdsCode },
-    links: { originalMessageUrl }
-  } = data;
-
-  const mhsJsonFragment = await retrieveMessageFromRepo(originalMessageUrl);
-  const practiceAsid = await getPracticeAsid(recipientOdsCode, serviceId);
-  console.log('just for lint: ' + mhsJsonFragment + practiceAsid);
-  await sendMessage({
-    interactionId: COPC_INTERACTION_ID,
-    conversationId,
-    odsCode: recipientOdsCode,
-    // WIP stubbed
-    message: '<' + COPC_INTERACTION_ID + ' 26a541ce-a5ab-4713-99a4-150ec3da25c6' + '200000000678',
-    attachments: [],
-    external_attachments: []
-  });
-
-  res.sendStatus(204);
-};
 
 healthRecordTransferRouter.post(
   '/fragment',
