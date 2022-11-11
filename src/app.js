279227491a8415e82d7a33db1a9f14d2920a03c9
Shruthi Chandekar <shruthi.chandekar1@nhs.net>
2022-11-11 16:09:16 +0000

    [PRMT-2892][API-01] Applied rate limiter to all the api endpoints.

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ import { options } from './config/logging';
 import * as logging from './middleware/logging';
 import swaggerDocument from './swagger.json';
 import helmet from 'helmet';
+import { rateLimiter } from './middleware/rateLimiter';
 
 const app = express();
 
@@ -21,6 +22,7 @@ app.use(
     maxAge: 31536000
   })
 );
+app.use(rateLimiter);
 app.use('/health', logging.middleware, healthCheck);
 app.use('/error', logging.middleware, error);
 app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
