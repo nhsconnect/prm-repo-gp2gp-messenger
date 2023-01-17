1388d8b95b38c04015b5ef1a2749d39e96dc59af
syedsadiqali <syed.ali69@nhs.net>
2023-01-17 11:34:02 +0000

    [PRMT-2722] Handled request body in test

--- src/api/ehr-out/__tests__/ehr-out-transfers.test.js
+++ src/api/ehr-out/__tests__/ehr-out-transfers.test.js
@@ -1,49 +1,55 @@
-import {getPracticeAsid} from '../../../services/fhir/sds-fhir-client';
+import { getPracticeAsid } from '../../../services/fhir/sds-fhir-client';
 import request from 'supertest';
-import {v4} from '../../../__mocks__/uuid';
+import { v4 } from '../../../__mocks__/uuid';
 import app from '../../../app';
 
 jest.mock('../../../services/fhir/sds-fhir-client');
 jest.mock('../../../config', () => ({
-    initializeConfig: jest.fn().mockReturnValue({
-        consumerApiKeys: {TEST_USER: 'correct-key'}
-    })
+  initializeConfig: jest.fn().mockReturnValue({
+    consumerApiKeys: { TEST_USER: 'correct-key' }
+  })
 }));
 
 const authKey = 'correct-key';
 const conversationId = v4();
+const mockRequestBodyWithMissingPayload = {
+  conversationId: conversationId,
+  odsCode: 'testOdsCode',
+  ehrRequestId: 'ehrRequestId',
+  coreEhr: 'core ehr stored in ehr repository'
+};
+
 const mockRequestBody = {
-    conversationId: conversationId,
-    odsCode: 'testOdsCode',
-    ehrRequestId: 'ehrRequestId',
-    coreEhr: 'core ehr stored in ehr repository'
+  conversationId: conversationId,
+  odsCode: 'testOdsCode',
+  ehrRequestId: 'ehrRequestId',
+  coreEhr: { ebXML: 'ebxml', payload: 'payload', attachments: [] }
 };
 describe('ehr out transfers', () => {
-    const odsCode = 'testOdsCode';
-    const interactionId = 'RCMR_IN030000UK06';
-    const serviceId = `urn:nhs:names:services:gp2gp:${interactionId}`;
-    it('should call getPracticeAsid', async () => {
-        const res = await request(app)
-            .post('/ehr-out-transfers/core')
-            .set('Authorization', authKey)
-            .send(mockRequestBody);
-        expect(res.status).toBe(204);
-        expect(getPracticeAsid).toHaveBeenCalledWith(odsCode, serviceId);
+  const odsCode = 'testOdsCode';
+  const interactionId = 'RCMR_IN030000UK06';
+  const serviceId = `urn:nhs:names:services:gp2gp:${interactionId}`;
+  it('should call getPracticeAsid', async () => {
+    const res = await request(app)
+      .post('/ehr-out-transfers/core')
+      .set('Authorization', authKey)
+      .send(mockRequestBody);
+    expect(res.status).toBe(204);
+    expect(getPracticeAsid).toHaveBeenCalledWith(odsCode, serviceId);
+  });
+
+  it('should throw an error when there is no payload in the ehr core message', async () => {
+    const res = await request(app)
+      .post('/ehr-out-transfers/core')
+      .set('Authorization', authKey)
+      .send(mockRequestBodyWithMissingPayload);
+
+    expect(res.status).toBe(503);
+    expect(res.body).toEqual({
+      errors: [
+        'Sending EHR Extract failed',
+        'Could not extract payload from the JSON message stored in EHR Repo'
+      ]
     });
-
-    it('should throw an error when there is no payload in the ehr core message', async () => {
-            const res = await request(app)
-                .post('/ehr-out-transfers/core')
-                .set('Authorization', authKey)
-                .send(mockRequestBody);
-
-            expect(res.status).toBe(503)
-            expect(res.body).toEqual({
-                errors: [
-                    'Sending EHR Extract failed',
-                    'Could not extract payload from the JSON message stored in EHR Repo'
-                ]
-            });
-        }
-    )
+  });
 });
--- src/api/ehr-out/ehr-out-transfers.js
+++ src/api/ehr-out/ehr-out-transfers.js
@@ -1,27 +1,26 @@
-import {setCurrentSpanAttributes} from '../../config/tracing';
-import {getPracticeAsid} from '../../services/fhir/sds-fhir-client';
-import {logError, logInfo} from '../../middleware/logging';
+import { setCurrentSpanAttributes } from '../../config/tracing';
+import { getPracticeAsid } from '../../services/fhir/sds-fhir-client';
+import { logError, logInfo } from '../../middleware/logging';
 
 export const ehrOutTransfers = async (req, res) => {
-    const {conversationId, odsCode, coreEhr} = req.body;
-    const interactionId = 'RCMR_IN030000UK06';
-    const serviceId = `urn:nhs:names:services:gp2gp:${interactionId}`;
-    setCurrentSpanAttributes({conversationId});
+  const { conversationId, odsCode, coreEhr } = req.body;
+  const interactionId = 'RCMR_IN030000UK06';
+  const serviceId = `urn:nhs:names:services:gp2gp:${interactionId}`;
+  setCurrentSpanAttributes({ conversationId });
 
-    try {
+  try {
+    logInfo('Getting asid for practice');
+    const asid = await getPracticeAsid(odsCode, serviceId); //need to rename the variable to receivingPracticeAcid
+    logInfo('Got asid for practice and its ' + asid);
 
-        logInfo('Getting asid for practice');
-        const asid = await getPracticeAsid(odsCode, serviceId); //need to rename the variable to receivingPracticeAcid
-        logInfo('Got asid for practice and its ' + asid);
-
-        const hl7Ehr = coreEhr.payload;
-        if (!hl7Ehr) {
-            throw new Error('Could not extract payload from the JSON message stored in EHR Repo');
-        }
-
-        res.sendStatus(204);
-    } catch (err) {
-        logError('Sending EHR Extract failed', {error: err.message});
-        res.status(503).send({errors: ['Sending EHR Extract failed', err.message]});
+    const hl7Ehr = coreEhr.payload;
+    if (!hl7Ehr) {
+      throw new Error('Could not extract payload from the JSON message stored in EHR Repo');
     }
+
+    res.sendStatus(204);
+  } catch (err) {
+    logError('Sending EHR Extract failed', { error: err.message });
+    res.status(503).send({ errors: ['Sending EHR Extract failed', err.message] });
+  }
 };
