705373b3e3b8bd62475fea2c7581593e826071a4
Joe Fong <joe.fong1@nhs.net>
2023-06-26 12:05:34 +0100

    [PRMT-3433] Add logging util and unit test

---
+++ src/services/mhs/__tests__/logging-utils.test.js
@@ -0,0 +1,122 @@
+import { readFileSync } from 'fs';
+import path from 'path';
+import cloneDeep from 'lodash.clonedeep';
+import { LOG_SIZE_LIMIT, logLargeMessage, removeBase64Payloads } from '../logging-utils';
+import { logInfo } from '../../../middleware/logging';
+
+jest.mock('../../../middleware/logging');
+
+const loadTestData = filename => {
+  return JSON.parse(readFileSync(path.join(__dirname, 'data', filename), 'utf8'));
+};
+describe('logging-utils', () => {
+  describe('removeBase64Payloads', () => {
+    it('should take a javascript object and redact any base64 encoded contents', () => {
+      // given
+      const inputObject = loadTestData('inputMessageContainingBase64.json');
+      const inputObjectClone = cloneDeep(inputObject);
+
+      const expectedOutputObject = loadTestData('expectedOutputWithBase64PayloadsRemoved.json');
+
+      // when
+      const output = removeBase64Payloads(inputObject);
+
+      // then
+      expect(output).toEqual(expectedOutputObject);
+
+      // verify that input object is not mutated in the process
+      expect(inputObject).toEqual(inputObjectClone);
+    });
+
+    it('should leave the input unchanged if it is not a JS object or an array', () => {
+      // given
+      const testInputs = [1234, 'a test string', true];
+
+      testInputs.forEach(input => {
+        const output = removeBase64Payloads(input);
+        expect(output).toBe(input);
+      });
+    });
+  });
+
+  describe('logLargeMessage', () => {
+    it('should log a message of < 256 KB normally', () => {
+      // given
+      const inputMessage = {
+        payload: '<COPC_IN000001UK01><content></content></COPC_IN000001UK01>',
+        attachments: [
+          {
+            payload: '[REMOVED]',
+            is_base64: true,
+            content_id: 'Attachment1@e-mis.com/EMISWeb/GP2GP2.2A',
+            content_type: 'text/plain'
+          }
+        ],
+        external_attachments: [
+          {
+            document_id: '_D695F543-7F04-4B27-81DC-AA238831DEA9',
+            message_id: '55146806-4E3B-4EFB-92BF-66556846FF2F',
+            description:
+              'Filename="D695F543-7F04-4B27-81DC-AA238831DEA9_IMG_01.jpg" ContentType=image/jpeg Compressed=No LargeAttachment=Yes OriginalBase64=No Length=6038724',
+            title: '"D695F543-7F04-4B27-81DC-AA238831DEA9_IMG_01.jpg"'
+          }
+        ]
+      };
+
+      // when
+      logLargeMessage(inputMessage);
+
+      // then
+      expect(logInfo).toHaveBeenCalledWith(inputMessage);
+    });
+
+    it('should log a message of > 256 KB in multiple parts', () => {
+      // given
+      const veryLongString =
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'.repeat(
+          25000
+        );
+      const inputMessage = {
+        payload: `<COPC_IN000001UK01><content>${veryLongString}</content></COPC_IN000001UK01>`,
+        attachments: [
+          {
+            payload: 'some_non_base64_payload',
+            is_base64: false,
+            content_id: 'Attachment1@e-mis.com/EMISWeb/GP2GP2.2A',
+            content_type: 'text/plain'
+          }
+        ],
+        external_attachments: [
+          {
+            document_id: '_D695F543-7F04-4B27-81DC-AA238831DEA9',
+            message_id: '55146806-4E3B-4EFB-92BF-66556846FF2F',
+            description:
+              'Filename="D695F543-7F04-4B27-81DC-AA238831DEA9_IMG_01.jpg" ContentType=image/jpeg Compressed=No LargeAttachment=Yes OriginalBase64=No Length=6038724',
+            title: '"D695F543-7F04-4B27-81DC-AA238831DEA9_IMG_01.jpg"'
+          }
+        ]
+      };
+
+      // when
+      logLargeMessage(inputMessage);
+
+      // then
+      const expectedNumberOfParts = Math.ceil(veryLongString.length / LOG_SIZE_LIMIT);
+      expect(logInfo).toHaveBeenCalledTimes(expectedNumberOfParts + 2); // including every part and the start and end logs
+
+      const loggedMessageParts = logInfo.mock.calls.slice(1, -1).map(array => array[0]);
+      expect(loggedMessageParts[0]).toMatch(
+        new RegExp(
+          `Part 0 of ${expectedNumberOfParts}: {"payload":"<COPC_IN000001UK01><content>Lorem ipsum`
+        )
+      );
+
+      // verify that we can get back the original message by combining every part of the logs
+      const combinedParts = loggedMessageParts
+        .map(str => str.replace(/Part \d+ of \d+: /, ''))
+        .join('');
+      const recoveredMessage = JSON.parse(combinedParts);
+      expect(recoveredMessage).toEqual(inputMessage);
+    });
+  });
+});
---
+++ src/services/mhs/logging-utils.js
@@ -0,0 +1,90 @@
+import { logInfo } from '../../middleware/logging';
+
+export const LOG_SIZE_LIMIT = 256 * 1024; // the 256 KB size limit of cloudWatch
+
+export const logOutboundMessage = (message, loggerFunction) => {
+  /*
+  Helper function to log an outbound message which could possibly be over the size limit of cloudWatch
+  Internally it is just composing removeBase64Payloads -> logLargeMessage.
+  Accept an optional input `loggerFunction` in case we want to log in a different level than INFO
+   */
+
+  // If `loggerFunction` is not given or not a function, will use the default logInfo to log the message
+  if (typeof loggerFunction !== 'function') {
+    loggerFunction = logInfo;
+  }
+
+  if (typeof message !== 'object') {
+    logInfo(
+      'logOutboundMessage got an input which is not a JS object. It may not work as expected.'
+    );
+  }
+
+  logLargeMessage(removeBase64Payloads(message), loggerFunction);
+};
+export const removeBase64Payloads = inputObject => {
+  /*
+    Recursively drill down the input object and remove any base64 payloads found.
+    The initial input is assumed to be a JS object or an array containing JS objects.
+   */
+  if (inputObject === null || typeof inputObject !== 'object') {
+    return inputObject;
+  }
+
+  if (Array.isArray(inputObject)) {
+    return inputObject.map(removeBase64Payloads);
+  }
+
+  const result = {};
+  for (let key in inputObject) {
+    result[key] = removeBase64Payloads(inputObject[key]);
+  }
+
+  if (inputObject?.is_base64 === true && 'payload' in inputObject) {
+    result['payload'] = '[REMOVED]';
+  }
+
+  return result;
+};
+
+export const logLargeMessage = (message, loggerFunction) => {
+  /*
+    A logging function that will try to keep a long message intact.
+    If given message size is larger than 256KB, it will split the message into multiple lines,
+    so that the full log is kept intact and not truncated by size limit.
+    The input message is assumed to be a JS object.
+ */
+
+  // If `loggerFunction` is not given or not a function, will use the default logInfo to log the message
+  if (typeof loggerFunction !== 'function') {
+    loggerFunction = logInfo;
+  }
+
+  const messageAsString = JSON.stringify(message);
+  if (messageAsString.length < LOG_SIZE_LIMIT) {
+    loggerFunction(message);
+  } else {
+    logMessageInMultipleParts(messageAsString, loggerFunction);
+  }
+};
+
+const logMessageInMultipleParts = (messageAsString, loggerFunction) => {
+  /*
+  Split a long message in multiple parts and log each of them in a human-readable and recoverable way.
+  Give a number to each part as logging in cloudwatch may be not in right order
+*/
+
+  const lineLength = LOG_SIZE_LIMIT - 50; // leave some space for metadata "Part x of y"
+  const numberOfParts = Math.ceil(messageAsString.length / lineLength);
+  loggerFunction(
+    `Received a message larger than the size limit of a single log. Attempt to log it in ${numberOfParts} parts`
+  );
+
+  for (let i = 0; i < numberOfParts; i++) {
+    const startIndex = i * lineLength;
+    const stringPart = messageAsString.slice(startIndex, startIndex + lineLength);
+    loggerFunction(`Part ${i} of ${numberOfParts}: ${stringPart}`);
+  }
+
+  loggerFunction(`Finished logging all parts of the message.`);
+};
