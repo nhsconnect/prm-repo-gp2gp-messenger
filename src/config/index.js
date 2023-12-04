530e0cf5ef610f65c43c77f1182ec27428043b2f
Mohammad Iqbal <mohammad.iqbal27@nhs.net>
2023-12-04 16:30:09 +0000

    Modified NHS_NUMBER_PREFIX to 'false' to temporarily process real patients

--- src/config/index.js
+++ src/config/index.js
@@ -6,7 +6,7 @@ export const initializeConfig = () => ({
   pdsAsid: process.env.PDS_ASID,
   mhsOutboundUrl: process.env.GP2GP_MESSENGER_MHS_OUTBOUND_URL,
   nhsEnvironment: process.env.NHS_ENVIRONMENT || 'local',
-  nhsNumberPrefix: process.env.NHS_NUMBER_PREFIX,
+  nhsNumberPrefix: false, // TODO: Updated to "false" in order to process real patients. Need a better solution than this.
   sdsFhirApiKey: process.env.SDS_FHIR_API_KEY,
   sdsFhirUrl: process.env.SDS_FHIR_URL,
   spineOrgCode: process.env.SPINE_ORG_CODE,
