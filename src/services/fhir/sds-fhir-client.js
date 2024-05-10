1d2ec9ca1a8e1ce05a19f44c09e03f1d7ce1bb7c
martin-nhs <martin.taylor22@nhs.net>
2024-05-10 15:43:10 +0100

    [PRMT-4832] Added logic to allow for dynamically retrieving the ASID.

--- src/services/fhir/sds-fhir-client.js
+++ src/services/fhir/sds-fhir-client.js
@@ -1,61 +1,45 @@
 import axios from 'axios';
 import { initializeConfig } from '../../config';
-import { logError, logInfo } from '../../middleware/logging';
+import { logInfo } from '../../middleware/logging';
 
 export const getPracticeAsid = async (odsCode, serviceId) => {
-  const { sdsFhirUrl, sdsFhirApiKey } = initializeConfig();
-  logInfo(`Getting ASID via FHIR for ODS code ${odsCode}`);
-  try {
-    const response = await axios.get(`${sdsFhirUrl}/Device`, {
-      params: {
-        organization: `https://fhir.nhs.uk/Id/ods-organization-code|${odsCode}`,
-        identifier: `https://fhir.nhs.uk/Id/nhsServiceInteractionId|${serviceId}`
-      },
-      headers: {
-        apiKey: sdsFhirApiKey
+  logInfo(`Attempting to query SDS in regards to ODS code ${odsCode} to fetch the practice ASID`);
+
+  const sdsQueryResponse = await querySds(odsCode, serviceId);
+  const entries = sdsQueryResponse.data.entry;
+  let asidCode = undefined;
+
+  entries.forEach(entry => {
+    entry.resource.identifier.forEach(identifier => {
+      if (
+        identifier.system === 'https://fhir.nhs.uk/Id/nhsMhsPartyKey' &&
+        identifier.value.includes(`${odsCode}-`)
+      ) {
+        // If the identifier matches, store the ASID value
+        asidCode = entry.resource.identifier.find(
+          id => id.system === 'https://fhir.nhs.uk/Id/nhsSpineASID'
+        )?.value;
       }
     });
+  });
 
-    const entries = response.data.entry;
-
-    if (entries.length === 0) {
-      throw new Error(`No ASID entries found for ODS code ${odsCode}`);
-    }
-
-    if (entries.length > 1) {
-      throw new Error(`Multiple ASID entries found for ODS code ${odsCode}`);
-    }
-
-    const identifiers = entries[0].resource.identifier;
-    const asidIdentifier = identifiers.filter(
-      identifier => identifier.system === 'https://fhir.nhs.uk/Id/nhsSpineASID'
-    );
-
-    if (asidIdentifier.length === 0) {
-      throw new Error(`No ASID identifier found for ODS code ${odsCode}`);
-    }
-
-    if (asidIdentifier.length > 1) {
-      throw new Error(`Multiple ASID identifiers found for ODS code ${odsCode}`);
-    }
-
-    const asidCode = asidIdentifier[0].value;
-    logInfo(`Successfully retrieved ASID: ${asidCode} via FHIR for ODS code ${odsCode}`);
-    return asidCode;
-  } catch (err) {
-    logError(
-      `Failed to retrieve ASID from FHIR for ODS Code: ${odsCode} - error: ${
-        err.message ? err.message : 'No Error Message'
-      }`
-    );
-    if (err.response) {
-      logError(
-        `Error: Request failed with non-2xx status code\n
-        Response body: ${err.response.data}\n
-        HTTP Status code: ${err.response.status}`
-      );
-    }
-
-    throw err;
+  if (asidCode === undefined) {
+    throw Error(`An ASID code couldn't be found for ODS code ${odsCode}`);
   }
+
+  return asidCode;
+};
+
+const querySds = async (odsCode, serviceId) => {
+  const { sdsFhirUrl, sdsFhirApiKey } = initializeConfig();
+
+  return await axios.get(`${sdsFhirUrl}/Device`, {
+    params: {
+      organization: `https://fhir.nhs.uk/Id/ods-organization-code|${odsCode}`,
+      identifier: `https://fhir.nhs.uk/Id/nhsServiceInteractionId|${serviceId}`
+    },
+    headers: {
+      apiKey: sdsFhirApiKey
+    }
+  });
 };
