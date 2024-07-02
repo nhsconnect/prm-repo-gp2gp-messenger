6538ebcfb4ce714069fe31dc6abc03abff34ecef
Andy Flint <andrew.flint@answerdigital.com>
2024-07-02 16:38:23 +0100

    [PRMP-534] Negative ACK POC

--- src/templates/generate-ehr-acknowledgement.js
+++ src/templates/generate-ehr-acknowledgement.js
@@ -1,27 +1,65 @@
 import dateFormat from 'dateformat';
+import {logInfo} from "../middleware/logging";
 
 export const buildEhrAcknowledgementPayload = ({
   acknowledgementMessageId,
   receivingAsid,
   sendingAsid,
-  acknowledgedMessageId
+  acknowledgedMessageId,
+  errorCode
 }) => {
   const timestamp = dateFormat(Date.now(), 'yyyymmddHHMMss');
+  let acknowledgementTypeCode;
+  let controlActEventReason;
+
+  switch (errorCode) {
+    case null:
+      logInfo('Building positive acknowledgement message with typeCode: AA');
+      acknowledgementTypeCode = 'AA'; // positive ACK
+      break;
+    case '30':
+    case '99':
+      logInfo(`Building negative acknowledgement message with typeCode: AE and errorCode: ${errorCode}`);
+      acknowledgementTypeCode = 'AE'; // negative ACK, unknown failure reason
+      controlActEventReason = controlActEventReasonTemplate(errorCode);
+      break;
+    default: // negative ACK, known failure reason
+      logInfo(`Building negative acknowledgement message with typeCode: AR and errorCode: ${errorCode}`);
+      acknowledgementTypeCode = 'AR';
+      controlActEventReason = controlActEventReasonTemplate(errorCode);
+  }
+
   return ackMessageTemplate({
     acknowledgementMessageId: acknowledgementMessageId.toUpperCase(),
     timestamp,
+    acknowledgementTypeCode: acknowledgementTypeCode,
+    acknowledgedMessageId: acknowledgedMessageId.toUpperCase(),
     receivingAsid,
     sendingAsid,
-    acknowledgedMessageId: acknowledgedMessageId.toUpperCase()
+    controlActEventReason
   });
 };
+
+const controlActEventReasonTemplate = errorCode =>
+  // TODO PRMP-534 identify what the 'codeSystem' codes need to be. is it the sending/receiving ASID?
+  `<reason typeCode="RSON">
+     <justifyingDetectedIssueEvent classCode="ALRT" moodCode="EVN">
+        <code code="${errorCode}" codeSystem="2.16.840.1.113883.2.1.3.2.4.17.101">
+           <qualifier>
+              <value code="ER" codeSystem="2.16.840.1.113883.2.1.3.2.4.17.104" />
+           </qualifier>
+        </code>
+     </justifyingDetectedIssueEvent>
+  </reason>`;
 
 const ackMessageTemplate = ({
   acknowledgementMessageId,
   timestamp,
+  acknowledgementTypeCode,
+  acknowledgedMessageId,
   receivingAsid,
   sendingAsid,
-  acknowledgedMessageId
+  controlActEventReason // will be null in the case of positive ack
 }) =>
   `<MCCI_IN010000UK13 xmlns="urn:hl7-org:v3" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="urn:hl7-org:v3 ..SchemasMCCI_IN010000UK13.xsd">
    <id root="${acknowledgementMessageId}" />
@@ -31,7 +69,7 @@ const ackMessageTemplate = ({
    <processingCode code="P" />
    <processingModeCode code="T" />
    <acceptAckCode code="NE" />
-   <acknowledgement typeCode="AA">
+   <acknowledgement typeCode=${acknowledgementTypeCode}>
       <messageRef>
          <id root="${acknowledgedMessageId}" />
       </messageRef>
@@ -47,6 +85,7 @@ const ackMessageTemplate = ({
       </device>
    </communicationFunctionSnd>
    <ControlActEvent classCode="CACT" moodCode="EVN">
+      ${controlActEventReason}
       <author1 typeCode="AUT">
          <AgentSystemSDS classCode="AGNT">
             <agentSystemSDS classCode="DEV" determinerCode="INSTANCE">
