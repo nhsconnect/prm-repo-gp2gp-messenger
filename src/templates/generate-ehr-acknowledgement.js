c215df095dc1d831df140e77924a7ef8176a50dc
chrisbloe-nhse <151756494+chrisbloe-nhse@users.noreply.github.com>
2024-07-17 13:19:20 +0100

    [PRMP-534] Formatting fixes (#81)
    Co-authored-by: Kris Bloe <kris.bloe@answerdigital.com>

--- src/templates/generate-ehr-acknowledgement.js
+++ src/templates/generate-ehr-acknowledgement.js
@@ -15,56 +15,56 @@ export const buildEhrAcknowledgementPayload = ({
       logInfo('Building positive acknowledgement message with typeCode: AA');
 
       return positiveAckMessageTemplate({
-         acknowledgementMessageId: acknowledgementMessageId.toUpperCase(),
-         timestamp: dateFormat(Date.now(), 'yyyymmddHHMMss'),
-         acknowledgementTypeCode: 'AA', // positive ACK
-         acknowledgedMessageId: acknowledgedMessageId.toUpperCase(),
-         receivingAsid,
-         sendingAsid
-       });
+        acknowledgementMessageId: acknowledgementMessageId.toUpperCase(),
+        timestamp: dateFormat(Date.now(), 'yyyymmddHHMMss'),
+        acknowledgementTypeCode: 'AA', // positive ACK
+        acknowledgedMessageId: acknowledgedMessageId.toUpperCase(),
+        receivingAsid,
+        sendingAsid
+      });
     case '30': // negative ACK, unknown failure reason
     case '99': // negative ACK, unknown failure reason
       logInfo(
         `Building negative acknowledgement message with typeCode: AE, errorCode: ${errorCode} and errorDisplayName: ${errorDisplayName}`
       );
-      
+
       return negativeAckMessageTemplate({
-         acknowledgementMessageId: acknowledgementMessageId.toUpperCase(),
-         timestamp: dateFormat(Date.now(), 'yyyymmddHHMMss'),
-         acknowledgementTypeCode: 'AE', // negative ACK, unknown failure reason
-         acknowledgedMessageId: acknowledgedMessageId.toUpperCase(),
-         receivingAsid,
-         sendingAsid,
-         errorCode,
-         errorDisplayName
+        acknowledgementMessageId: acknowledgementMessageId.toUpperCase(),
+        timestamp: dateFormat(Date.now(), 'yyyymmddHHMMss'),
+        acknowledgementTypeCode: 'AE', // negative ACK, unknown failure reason
+        acknowledgedMessageId: acknowledgedMessageId.toUpperCase(),
+        receivingAsid,
+        sendingAsid,
+        errorCode,
+        errorDisplayName
       });
     default: // negative ACK, known failure reason
       logInfo(
         `Building negative acknowledgement message with typeCode: AR and errorCode: ${errorCode} and errorDisplayName: ${errorDisplayName}`
       );
-      
+
       return negativeAckMessageTemplate({
-         acknowledgementMessageId: acknowledgementMessageId.toUpperCase(),
-         timestamp: dateFormat(Date.now(), 'yyyymmddHHMMss'),
-         acknowledgementTypeCode: 'AR', // negative ACK, known failure reason
-         acknowledgedMessageId: acknowledgedMessageId.toUpperCase(),
-         receivingAsid,
-         sendingAsid,
-         errorCode,
-         errorDisplayName
+        acknowledgementMessageId: acknowledgementMessageId.toUpperCase(),
+        timestamp: dateFormat(Date.now(), 'yyyymmddHHMMss'),
+        acknowledgementTypeCode: 'AR', // negative ACK, known failure reason
+        acknowledgedMessageId: acknowledgedMessageId.toUpperCase(),
+        receivingAsid,
+        sendingAsid,
+        errorCode,
+        errorDisplayName
       });
   }
 };
 
 const positiveAckMessageTemplate = ({
-      acknowledgementMessageId,
-      timestamp,
-      acknowledgementTypeCode,
-      acknowledgedMessageId,
-      receivingAsid,
-      sendingAsid,
-   }) =>
-   `<MCCI_IN010000UK13 xmlns="urn:hl7-org:v3" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="urn:hl7-org:v3 ..SchemasMCCI_IN010000UK13.xsd">
+  acknowledgementMessageId,
+  timestamp,
+  acknowledgementTypeCode,
+  acknowledgedMessageId,
+  receivingAsid,
+  sendingAsid
+}) =>
+  `<MCCI_IN010000UK13 xmlns="urn:hl7-org:v3" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="urn:hl7-org:v3 ..SchemasMCCI_IN010000UK13.xsd">
       <id root="${acknowledgementMessageId}" />
       <creationTime value="${timestamp}" />
       <versionCode code="V3NPfIT4.2.00" />
@@ -98,17 +98,17 @@ const positiveAckMessageTemplate = ({
       </ControlActEvent>
    </MCCI_IN010000UK13>`;
 
-   const negativeAckMessageTemplate = ({
-      acknowledgementMessageId,
-      timestamp,
-      acknowledgementTypeCode,
-      acknowledgedMessageId,
-      receivingAsid,
-      sendingAsid,
-      errorCode,             // not in a positive ack
-      errorDisplayName       // not in a positive ack
-   }) =>
-   `<MCCI_IN010000UK13 xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xs="http://www.w3.org/2001/XMLSchema" type="Message" xmlns="urn:hl7-org:v3">
+const negativeAckMessageTemplate = ({
+  acknowledgementMessageId,
+  timestamp,
+  acknowledgementTypeCode,
+  acknowledgedMessageId,
+  receivingAsid,
+  sendingAsid,
+  errorCode, // not in a positive ack
+  errorDisplayName // not in a positive ack
+}) =>
+  `<MCCI_IN010000UK13 xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xs="http://www.w3.org/2001/XMLSchema" type="Message" xmlns="urn:hl7-org:v3">
       <id root="${acknowledgementMessageId}" />
       <creationTime value="${timestamp}" />
       <versionCode code="V3NPfIT3.1.10" />
